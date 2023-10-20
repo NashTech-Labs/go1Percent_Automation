3224e8b670ce2056f352593e97aebb14f5c4439a
RahulKnolder <rahul.sinha@knolder.com>
2023-10-20 23:56:09 +0530

    added my initial test cases

--- go1percent/globals.js
+++ go1percent/globals.js
@@ -11,23 +11,43 @@ module.exports = {
         chromedriver.stop();
         done();
     },
-    headers:{
-        'authority': 'backend.qa.go1percent.com',
-        'accept': 'application/json, text/plain, */*',
-        'accept-language': 'en-US,en;q=0.9',
-        'authorization': 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJIRzk4ejl1eFN0T3VPaVg4a2RiYlNPblQ2a29OWUlCSENLeXM2LUhTYnlFIn0.eyJleHAiOjE2OTc4MDkxMzksImlhdCI6MTY5NzgwNzMzOSwiYXV0aF90aW1lIjoxNjk3ODA3MzM4LCJqdGkiOiIwYzI0MGRlNS0wMWEwLTQzNGYtOTQ0MC1iMTQxNTcwN2VhMDUiLCJpc3MiOiJodHRwczovL2F1dGguZ28xcGVyY2VudC5jb20vYXV0aC9yZWFsbXMvbmFzaHRlY2giLCJhdWQiOlsibGVhZGVyYm9hcmQtdWkiLCJsZWFkZXJib2FyZC1kZXYtdWkiLCJhY2NvdW50Il0sInN1YiI6IjgzYjAzNGY1LTg2NzUtNGQ5NC04NDQ0LTg3ZjZlYTk4YzlhYiIsInR5cCI6IkJlYXJlciIsImF6cCI6ImxlYWRlcmJvYXJkLXFhLXVpIiwibm9uY2UiOiI3MTU5NGYyZC0yODhkLTQwY2MtOGI0MC02NGU4ZTI0ZmIzNjYiLCJzZXNzaW9uX3N0YXRlIjoiODkyYjUzYmQtMDcyNC00YjJhLWExNDItZWRiN2ExODA2NDg0IiwiYWNyIjoiMSIsImFsbG93ZWQtb3JpZ2lucyI6WyJodHRwczovL25hc2h0ZWNoZ2xvYmFsLnFhLmdvMXBlcmNlbnQuY29tIiwiaHR0cHM6Ly9sZWFkZXJib2FyZC1xYS5rbm9sZHVzLmNvbSIsImh0dHBzOi8va25vbGR1cy5xYS5nbzFwZXJjZW50LmNvbSIsImh0dHBzOi8vcmVwb3J0LnFhLmdvMXBlcmNlbnQuY29tIiwiaHR0cHM6Ly9sZWFkZXJib2FyZC1xYS5nbzFwZXJjZW50LmNvbSIsImh0dHA6Ly9sb2NhbGhvc3Q6ODA4OCIsImh0dHA6Ly9sb2NhbGhvc3Q6NDIwMCJdLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsib2ZmbGluZV9hY2Nlc3MiLCJkZWZhdWx0LXJvbGVzLW5hc2h0ZWNoIiwidW1hX2F1dGhvcml6YXRpb24iLCJlbXBsb3llZSJdfSwicmVzb3VyY2VfYWNjZXNzIjp7ImxlYWRlcmJvYXJkLXFhLXVpIjp7InJvbGVzIjpbImVtcGxveWVlIl19LCJsZWFkZXJib2FyZC11aSI6eyJyb2xlcyI6WyJlbXBsb3llZSJdfSwibGVhZGVyYm9hcmQtZGV2LXVpIjp7InJvbGVzIjpbImVtcGxveWVlIl19LCJhY2NvdW50Ijp7InJvbGVzIjpbIm1hbmFnZS1hY2NvdW50IiwibWFuYWdlLWFjY291bnQtbGlua3MiLCJ2aWV3LXByb2ZpbGUiXX19LCJzY29wZSI6Im9wZW5pZCBlbWFpbCBwcm9maWxlIiwic2lkIjoiODkyYjUzYmQtMDcyNC00YjJhLWExNDItZWRiN2ExODA2NDg0IiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJuYW1lIjoidGVzdCBlbXBsb3llZSIsInByZWZlcnJlZF91c2VybmFtZSI6InRlc3RlbXBsb3llZSIsImdpdmVuX25hbWUiOiJ0ZXN0IiwiZmFtaWx5X25hbWUiOiJlbXBsb3llZSIsImVtYWlsIjoidGVzdGVtcGxveWVlQG5hc2h0ZWNoZ2xvYmFsLmNvbSJ9.H5SAgY2YiPTjUUHpViGo9X7aN1Qdas9qZaNlVoRKV1ZnqLJIHViP0gn7HPq0_xGIePVrsQjqim-STOsx-8Yib8L_SKVQL9bGjIghOu-mVTWEYnkPIHA_lJvPfsN3VjAFoBHMU9i6nuT2py_6Ub7Kl69ivYFBj6YT7NziDMQ6-2qLG8ppNCsI4PEC_LyZMh60fsGYIzF0abxOhWW52H0Ej9GwnelBbutli7WpxEJpbrYN13zK89QLSS46QoZa_OWmGo-xkv4-aZXKHvq8jegrqED79RT1YDrKDo9HKlvWrOqhzzXkW65dB9bd_zrFMs7_qYJn4nHxWDJ6AlIHgfBa_g',
-        'languagevalue': 'en',
-        'origin': 'https://nashtechglobal.qa.go1percent.com',
-        'referer': 'https://nashtechglobal.qa.go1percent.com/',
-        'sec-ch-ua': '"Chromium";v="116", "Not)A;Brand";v="24", "Google Chrome";v="116"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"Linux"',
-        'sec-fetch-dest': 'empty',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-site': 'same-site',
-        'source': 'https://nashtechglobal.qa.go1percent.com',
-        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
+    // headers:{
+    //     'authority': 'backend.qa.go1percent.com',
+    //     'accept': 'application/json, text/plain, */*',
+    //     'accept-language': 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJIRzk4ejl1eFN0T3VPaVg4a2RiYlNPblQ2a29OWUlCSENLeXM2LUhTYnlFIn0.eyJleHAiOjE2OTc4MjAyNjQsImlhdCI6MTY5NzgxODQ2NCwiYXV0aF90aW1lIjoxNjk3ODE4NDYzLCJqdGkiOiI4ZjJmYmViYy05NmEwLTQ2NjEtOWMyZS1jODc3NGI5YmY1NTkiLCJpc3MiOiJodHRwczovL2F1dGguZ28xcGVyY2VudC5jb20vYXV0aC9yZWFsbXMvbmFzaHRlY2giLCJhdWQiOlsibGVhZGVyYm9hcmQtdWkiLCJsZWFkZXJib2FyZC1kZXYtdWkiLCJhY2NvdW50Il0sInN1YiI6IjgzYjAzNGY1LTg2NzUtNGQ5NC04NDQ0LTg3ZjZlYTk4YzlhYiIsInR5cCI6IkJlYXJlciIsImF6cCI6ImxlYWRlcmJvYXJkLXFhLXVpIiwibm9uY2UiOiIxMDk1YWY1YS04NjgzLTQ2MTMtODg3Mi1hNDU0OGJkMGMwYTgiLCJzZXNzaW9uX3N0YXRlIjoiZDFmY2VlZTAtZWE2Zi00ZTRjLWI4MTMtYzMyODg5ODlmMTgyIiwiYWNyIjoiMSIsImFsbG93ZWQtb3JpZ2lucyI6WyJodHRwczovL25hc2h0ZWNoZ2xvYmFsLnFhLmdvMXBlcmNlbnQuY29tIiwiaHR0cHM6Ly9sZWFkZXJib2FyZC1xYS5rbm9sZHVzLmNvbSIsImh0dHBzOi8va25vbGR1cy5xYS5nbzFwZXJjZW50LmNvbSIsImh0dHBzOi8vcmVwb3J0LnFhLmdvMXBlcmNlbnQuY29tIiwiaHR0cHM6Ly9sZWFkZXJib2FyZC1xYS5nbzFwZXJjZW50LmNvbSIsImh0dHA6Ly9sb2NhbGhvc3Q6ODA4OCIsImh0dHA6Ly9sb2NhbGhvc3Q6NDIwMCJdLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsib2ZmbGluZV9hY2Nlc3MiLCJkZWZhdWx0LXJvbGVzLW5hc2h0ZWNoIiwidW1hX2F1dGhvcml6YXRpb24iLCJlbXBsb3llZSJdfSwicmVzb3VyY2VfYWNjZXNzIjp7ImxlYWRlcmJvYXJkLXFhLXVpIjp7InJvbGVzIjpbImVtcGxveWVlIl19LCJsZWFkZXJib2FyZC11aSI6eyJyb2xlcyI6WyJlbXBsb3llZSJdfSwibGVhZGVyYm9hcmQtZGV2LXVpIjp7InJvbGVzIjpbImVtcGxveWVlIl19LCJhY2NvdW50Ijp7InJvbGVzIjpbIm1hbmFnZS1hY2NvdW50IiwibWFuYWdlLWFjY291bnQtbGlua3MiLCJ2aWV3LXByb2ZpbGUiXX19LCJzY29wZSI6Im9wZW5pZCBlbWFpbCBwcm9maWxlIiwic2lkIjoiZDFmY2VlZTAtZWE2Zi00ZTRjLWI4MTMtYzMyODg5ODlmMTgyIiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJuYW1lIjoidGVzdCBlbXBsb3llZSIsInByZWZlcnJlZF91c2VybmFtZSI6InRlc3RlbXBsb3llZSIsImdpdmVuX25hbWUiOiJ0ZXN0IiwiZmFtaWx5X25hbWUiOiJlbXBsb3llZSIsImVtYWlsIjoidGVzdGVtcGxveWVlQG5hc2h0ZWNoZ2xvYmFsLmNvbSJ9.byGZr8XkgdbGUS7PItuApsldleAG3cdppRJWveL4jO8XVpExSA-uOAhcPfGZr2PEOv5ZJLHtMVJe8bpiC1Nb90yx1WfwmZBGIQHKf7yJUurngmWxtncwzPZHZOFmnrXldHO5TLs_SHEAwriz4QPHefTQV57msEOyLSQpVTn7O_rE4ZaCzh7AvYhS0_M7fpJt9saxdQ3ktyjAnv393T595In9jdsNcaHGwLBlAjTMt3FPiJ_ToMpTk7EzNyjpKFRsRQ3MqrT2AlNqoayoSmtwgBsQZp7GTzavwmtU0GFqsppbSbLxpjTY2k0cU_Blz7obs3Xfy5ujFWvwbMYefGgC2Q',
+    //     'languagevalue': 'en',
+    //     'origin': 'https://nashtechglobal.qa.go1percent.com',
+    //     'referer': 'https://nashtechglobal.qa.go1percent.com/',
+    //     'sec-ch-ua': '"Chromium";v="116", "Not)A;Brand";v="24", "Google Chrome";v="116"',
+    //     'sec-ch-ua-mobile': '?0',
+    //     'sec-ch-ua-platform': '"Linux"',
+    //     'sec-fetch-dest': 'empty',
+    //     'sec-fetch-mode': 'cors',
+    //     'sec-fetch-site': 'same-site',
+    //     'source': 'https://nashtechglobal.qa.go1percent.com',
+    //     'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
+    //   }
+      headers:{
+        
+            "authority": "backend.qa.go1percent.com",
+            "accept": "application/json, text/plain, */*",
+            "accept-language": "en-US,en;q=0.9",
+            "authorization": "Bearer eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJIRzk4ejl1eFN0T3VPaVg4a2RiYlNPblQ2a29OWUlCSENLeXM2LUhTYnlFIn0.eyJleHAiOjE2OTc4MjU5NTMsImlhdCI6MTY5NzgyNDE1MywiYXV0aF90aW1lIjoxNjk3ODI0MTUyLCJqdGkiOiIxMjRjNDhhYy1jNWI2LTQ5YTItYTcwNi1lYmM3MWRmZWQ5ODQiLCJpc3MiOiJodHRwczovL2F1dGguZ28xcGVyY2VudC5jb20vYXV0aC9yZWFsbXMvbmFzaHRlY2giLCJhdWQiOlsibGVhZGVyYm9hcmQtdWkiLCJsZWFkZXJib2FyZC1kZXYtdWkiLCJhY2NvdW50Il0sInN1YiI6IjgzYjAzNGY1LTg2NzUtNGQ5NC04NDQ0LTg3ZjZlYTk4YzlhYiIsInR5cCI6IkJlYXJlciIsImF6cCI6ImxlYWRlcmJvYXJkLXFhLXVpIiwibm9uY2UiOiJjMmY0OTZiYy05NjM5LTRhMDctOTY4MS05YjNjOGIzY2IzMDAiLCJzZXNzaW9uX3N0YXRlIjoiYjYwYzk0OTAtNzMwYi00Y2Q4LWJiZjktMjk1ZjQ3M2FkMDk1IiwiYWNyIjoiMSIsImFsbG93ZWQtb3JpZ2lucyI6WyJodHRwczovL25hc2h0ZWNoZ2xvYmFsLnFhLmdvMXBlcmNlbnQuY29tIiwiaHR0cHM6Ly9sZWFkZXJib2FyZC1xYS5rbm9sZHVzLmNvbSIsImh0dHBzOi8va25vbGR1cy5xYS5nbzFwZXJjZW50LmNvbSIsImh0dHBzOi8vcmVwb3J0LnFhLmdvMXBlcmNlbnQuY29tIiwiaHR0cHM6Ly9sZWFkZXJib2FyZC1xYS5nbzFwZXJjZW50LmNvbSIsImh0dHA6Ly9sb2NhbGhvc3Q6ODA4OCIsImh0dHA6Ly9sb2NhbGhvc3Q6NDIwMCJdLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsib2ZmbGluZV9hY2Nlc3MiLCJkZWZhdWx0LXJvbGVzLW5hc2h0ZWNoIiwidW1hX2F1dGhvcml6YXRpb24iLCJlbXBsb3llZSJdfSwicmVzb3VyY2VfYWNjZXNzIjp7ImxlYWRlcmJvYXJkLXFhLXVpIjp7InJvbGVzIjpbImVtcGxveWVlIl19LCJsZWFkZXJib2FyZC11aSI6eyJyb2xlcyI6WyJlbXBsb3llZSJdfSwibGVhZGVyYm9hcmQtZGV2LXVpIjp7InJvbGVzIjpbImVtcGxveWVlIl19LCJhY2NvdW50Ijp7InJvbGVzIjpbIm1hbmFnZS1hY2NvdW50IiwibWFuYWdlLWFjY291bnQtbGlua3MiLCJ2aWV3LXByb2ZpbGUiXX19LCJzY29wZSI6Im9wZW5pZCBlbWFpbCBwcm9maWxlIiwic2lkIjoiYjYwYzk0OTAtNzMwYi00Y2Q4LWJiZjktMjk1ZjQ3M2FkMDk1IiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJuYW1lIjoidGVzdCBlbXBsb3llZSIsInByZWZlcnJlZF91c2VybmFtZSI6InRlc3RlbXBsb3llZSIsImdpdmVuX25hbWUiOiJ0ZXN0IiwiZmFtaWx5X25hbWUiOiJlbXBsb3llZSIsImVtYWlsIjoidGVzdGVtcGxveWVlQG5hc2h0ZWNoZ2xvYmFsLmNvbSJ9.HmMrpZhta4s2kcAi3POQuxScDOF45gXCoPvARLe3tID3pq9UpnY1RLAHNEnEIUtG2dp7hYRh4_MtJVC71ssEy6TzppLL1CHQrU3lnc5hXGk0P26MtGwhZMWlxSS80lvQ7edjGf0hf7kGygr7esehhVb_BuWZhydimXdlywcVL91mo5736kF4AHCQr3n6r1nRYtPhUNQe_ZsOobvjddqF1nHWbdoZVrgU0cMwxVVXkueZ5OvLYi-EDJD9I3-S5FE-lAS_e4IKhK4wX5nAROVDC7SGq9ZWxkwphxE8vh9pDGHzp0oG8voLldmsFk-moMlAW5iE38zc3U9MTXcEyZdlAQ",
+            "languagevalue": "en",
+            "origin": "https://nashtechglobal.qa.go1percent.com",
+            "referer": "https://nashtechglobal.qa.go1percent.com/",
+            "sec-ch-ua": "\"Chromium\";v=\"116\", \"Not)A;Brand\";v=\"24\", \"Google Chrome\";v=\"116\"",
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": "\"Linux\"",
+            "sec-fetch-dest": "empty",
+            "sec-fetch-mode": "cors",
+            "sec-fetch-site": "same-site",
+            "source": "https://nashtechglobal.qa.go1percent.com",
+            "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"
+          
+          
       }
       
+
 
 };
--- go1percent/test/api-testing/leaderboardSummaryApiTest.js
+++ go1percent/test/api-testing/leaderboardSummaryApiTest.js
@@ -1,37 +1,121 @@
 const global = require('../../globals');
 const headers = global.headers;
 
-
+/**
+ * Leaderboard API Testing
+ */
 describe('Leaderboard API Testing', function () {
   const baseUrl = 'https://backend.qa.go1percent.com';
 
   /**
-  * Test case to retrieve Leading Practices Summary from API.
-  * @param {Object} supertest - The supertest object for making HTTP requests.
-  */
-
+   * Test case to retrieve Leading Practices Summary from API.
+   * @param {Object} supertest - The supertest object for making HTTP requests.
+   */
   it('should retrieve Leading Practices Summary', async function ({ supertest }) {
-
     const response = await supertest
       .request(baseUrl)
       .get('/studios')
       .set(headers)
       .expect(200) // Expect a successful response with HTTP status code 200
-      .expect('Content-Type', /json/);
+      .expect('Content-Type', /json/); // Expect the response to have JSON content type
+
     const responseBody = response.body;
     expect(responseBody.length).to.be.greaterThan(0);
 
     for (let indexOfResponse = 0; indexOfResponse < responseBody.length; indexOfResponse++) {
-      //assertion to check the properties of the response body
-      expect(Object.keys(responseBody[indexOfResponse])).to.include('studioType');
-      expect(Object.keys(responseBody[indexOfResponse])).to.include('name');
-      expect(Object.keys(responseBody[indexOfResponse])).to.include('allTimeScore');
-      expect(Object.keys(responseBody[indexOfResponse])).to.include('studioHeadEmail');
-      expect(Object.keys(responseBody[indexOfResponse])).to.include('monthlyScore');
-      expect(Object.keys(responseBody[indexOfResponse])).to.include('memberAvg');
-      expect(Object.keys(responseBody[indexOfResponse])).to.include('studioHeadName');
+      const object = responseBody[indexOfResponse];
+
+      // Check if 'imageFile' is present in the object
+      if ('imageFile' in object) {
+        expect(object).to.have.all.keys(
+          'allTimeScore',
+          'id',
+          'imageFile',
+          'memberAllTimeAvg',
+          'memberAvg',
+          'memberCount',
+          'monthlyScore',
+          'name',
+          'studioHeadEmail',
+          'studioHeadName',
+          'studioType'
+        );
+        expect(object.imageFile).to.be.a('string');
+      } else {
+        // If 'imageFile' is not present, assert without it
+        expect(object).to.have.all.keys(
+          'allTimeScore',
+          'id',
+          'memberAllTimeAvg',
+          'memberAvg',
+          'memberCount',
+          'monthlyScore',
+          'name',
+          'studioHeadEmail',
+          'studioHeadName',
+          'studioType'
+        );
+      }
+
+      // Assert the types of each property's value
+      expect(object.studioType).to.be.a('string');
+      expect(object.name).to.be.a('string');
+      expect(object.allTimeScore).to.be.a('number');
+      expect(object.studioHeadEmail).to.be.a('string');
+      expect(object.monthlyScore).to.be.a('number');
+      expect(object.memberAvg).to.be.a('number'); // Make sure this should be a string
+      expect(object.memberAllTimeAvg).to.be.a('number'); // Add this assertion
+      expect(object.monthlyScore).to.be.a('number');
+      expect(object.memberAvg).to.be.a('number'); // Change the type to 'number'
+      expect(object.memberCount).to.be.a('number'); // Add this assertion
+
+      if (object.studioHeadName === null) {
+        expect(object.studioHeadName).to.be.null;
+      } else {
+        expect(object.studioHeadName).to.be.a('string');
+      }
     }
   });
 
- 
+
+  /**
+   * Test case to retrieve Leading Nasher data for all times.
+   * @param {Object} supertest - The supertest object for making HTTP requests.
+   */
+  it('should retrieve Leading Nasher data for all times', async function ({ supertest }) {
+    const response = await supertest
+      .request(baseUrl)
+      .get('/summary?period=alltime')
+      .set(headers)
+      .expect(200) // Expect a successful response with HTTP status code 200
+      .expect('Content-Type', /json/); // Expect the response to have JSON content type
+
+    // Assert that the response body has data
+    expect(response.body.length).to.be.greaterThan(0);
+
+    const responseBody = response.body;
+
+    for (let indexOfResponse = 0; indexOfResponse < responseBody.length; indexOfResponse++) {
+      const object = responseBody[indexOfResponse];
+
+      // Check if the object has the expected keys
+      expect(object).to.have.all.keys(
+        'knolderId',
+        'KnolderName',
+        'profilePic',
+        'studioName',
+        'score',
+        'rank'
+      );
+
+      // Assert the types of each property's value
+      expect(object.knolderId).to.be.a('number');
+      expect(object.KnolderName).to.be.a('string');
+      expect(object.profilePic).to.be.a('string');
+      expect(object.studioName).to.be.a('string');
+      expect(object.score).to.be.a('number');
+      expect(object.rank).to.be.a('number');
+    }
+  });
+
 });
