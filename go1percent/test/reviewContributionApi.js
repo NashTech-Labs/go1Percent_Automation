f175bd27c31f86c7cad73d0648e64af9f960a077
riyaknol11 <riya.sharma@knoldus.com>
2023-10-23 18:14:54 +0530

    modified the test cases for api testing

--- go1percent/test/reviewContributionApi.js
+++ go1percent/test/reviewContributionApi.js
@@ -1,12 +1,14 @@
 const headers = require('../globals')
+
+
 describe('Review Contribution API Testing', function () {
 
     /**
- * Test case to retrieve all the pending contributions for approval
- * @param {object} supertest - The supertest object for making HTTP requests.
- */
+     * Test case to retrieve all the pending contributions for approval or rejection on Approvals page.
+     * @param {object} supertest - The supertest object for making HTTP requests.
+     */
 
-    it('should retrieve all the pending contributions for approval', async function ({ supertest }) {
+    it('should retrieve all the pending contributions on approvals page', async function ({ supertest }) {
 
         await supertest
 
@@ -21,16 +23,16 @@ describe('Review Contribution API Testing', function () {
             // Assertions to check the structure and properties of the response
             .then(function (response) {
 
-                expect(Object.keys(response.body.data).length).to.be.greaterThan(0);
-                expect(response.body.data._1[0]).to.have.property('contributionId');
-                expect(response.body.data._1[0]).to.have.property('knolderName');
-                expect(response.body.data._1[1]).to.have.property('title');
-                expect(response.body.data._1[1]).to.have.property('status');
+                let responseBody = response.body.data._1;
+                expect((responseBody).length).to.be.greaterThan(0);
+                expect(responseBody[0]).to.have.property('contributionId');
+                expect(responseBody[0]).to.have.property('knolderName');
+                expect(responseBody[1]).to.have.property('title');
+                expect(responseBody[1]).to.have.property('status');
 
                 // Assert that the status of every contribution should be "PENDING"
-                const responseData = response.body.data._1;
-                for (let i = 0; i < response.body.data._1.length; i++) {
-                    expect(responseData[i].status.name).to.equal('PENDING');
+                for (let i = 0; i < responseBody.length; i++) {
+                    expect(responseBody[i].status.name).to.equal('PENDING');
                 }
 
             });
@@ -38,11 +40,11 @@ describe('Review Contribution API Testing', function () {
     });
 
     /**
-     * Test case to approve or reject a contribution
+     * Test case to approve or reject a contribution -- PUT method call.
      * @param {object} supertest - The supertest object for making HTTP requests.
      */
 
-    it('approving/rejecting a contribution', async function ({ supertest }) {
+    it('should approve/reject a contribution', async function ({ supertest }) {
 
         await supertest
 
@@ -71,7 +73,47 @@ describe('Review Contribution API Testing', function () {
 
     });
 
-    it('should retrieve the details of a specific contribution on click', async function ({ supertest }) {
+    /**
+    * Test case to retrieve a list of the contributions on "All contributions" page.
+    * and validates the structure of the response and checks that the status of each contribution
+    * falls within the allowed values: "PENDING," "APPROVED," or "REJECTED."
+    */
+
+    it('should retrieve list of the contributions on "all contributions" page', async function ({ supertest }) {
+
+        await supertest
+            .request(headers.baseurl)
+            .get("/contribution/getApprovedContributionsForAll?pageNumber=1&limit=10")
+            .set(headers.headers)
+            .expect(200)
+
+            // Assertions to check the structure and properties of the response
+            .then(function (response) {
+
+                let responseBody = response.body.data._1;
+                expect((responseBody).length).to.be.greaterThan(0);
+                expect(responseBody[0]).to.have.property('contributionId');
+                expect(responseBody[0]).to.have.property('knolderName');
+                expect(responseBody[1]).to.have.property('title');
+                expect(responseBody[1]).to.have.property('status');
+
+                // Assert that the status of every contribution can be "PENDING," "APPROVED," or "REJECTED"
+                for (let i = 0; i < responseBody.length; i++) {
+                    const status = responseBody[i].status.name;
+                    expect(status).to.be.oneOf(['PENDING', 'APPROVED', 'REJECTED']);
+                }
+
+            });
+    });
+
+    /**
+     * This test case sends a GET request to fetch details of a specific contribution
+     * and validates the structure of the response and checks that the status of each contribution
+     * falls within the allowed values: "PENDING," "APPROVED," or "REJECTED."
+     * @param {Object} supertest - The supertest object for making HTTP requests.
+     */
+
+    it('should retrieve the details of a specific contribution', async function ({ supertest }) {
 
         await supertest
 
@@ -84,7 +126,7 @@ describe('Review Contribution API Testing', function () {
             .expect(200)
 
             .then(function (response) {
-                const responseBody = response.body.data;
+                let responseBody = response.body.data;
 
                 // Assert the keys and data types of the properties
                 for (let i = 0; i < responseBody.length; i++) {
@@ -96,24 +138,52 @@ describe('Review Contribution API Testing', function () {
                         'title',
                         'contributionDate',
                         'technologyDetails',
-                        'urlDetails',
-                        'status',
-                        'reviewer_name'
                     );
-
-                    expect(responseObject.contributionId).to.be.a('number');
-                    expect(responseObject.contributionType.name).to.be.a('string');
-                    expect(responseObject.title).to.be.a('string');
-                    expect(responseObject.contributionDate).to.be.a('string');
-                    expect(responseObject.technologyDetails).to.be.a('string');
-                    expect(responseObject.studioName).to.be.a('string');
-                    expect(responseObject.reviewer_name).to.be.a('string');
                 }
 
                 // Assert that the status of every contribution can be "PENDING," "APPROVED," or "REJECTED"
-                const responseData = response.body.data;
-                for (let i = 0; i < responseData.length; i++) {
-                    const status = responseData[i].status.name;
+                for (let i = 0; i < responseBody.length; i++) {
+                    const status = responseBody[i].status.name;
+                    expect(status).to.be.oneOf(['PENDING', 'APPROVED', 'REJECTED']);
+                }
+            });
+
+    });
+
+    /**
+    
+    * This test case sends a GET request to retrieve contributions from a specific date
+        and validates the structure of the response and checks that the status of each contribution
+     * falls within the allowed values: "PENDING," "APPROVED," or "REJECTED."
+     * @param {Object} supertest - The supertest object for making HTTP requests.
+     */
+
+    it('should retrieve the contributions on the basis of date', async function ({ supertest }) {
+        await supertest
+            .request(headers.baseurl)
+            .get("/contribution/allContribution?pageNumber=1&limit=10&date=2023-10-16")
+            .set(headers.headers)
+            .expect(200)
+
+            .then(function (response) {
+                const responseBody = response.body.data;
+
+                // Assert the keys of the properties in the response for each contribution
+                for (let i = 0; i < responseBody.length; i++) {
+                    const responseObject = responseBody[i];
+
+                    expect(responseObject).to.have.all.keys(
+                        'contributionId',
+                        'contributionType',
+                        'title',
+                        'contributionDate',
+                        'technologyDetails',
+                    );
+                }
+
+                // Assert that the status of every contribution can be "PENDING," "APPROVED," or "REJECTED"
+                for (let i = 0; i < responseBody.length; i++) {
+                    const status = responseBody[i].status.name;
                     expect(status).to.be.oneOf(['PENDING', 'APPROVED', 'REJECTED']);
                 }
             });
@@ -121,4 +191,3 @@ describe('Review Contribution API Testing', function () {
     });
 
 });
-
