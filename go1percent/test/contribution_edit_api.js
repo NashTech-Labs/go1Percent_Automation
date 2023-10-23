52d8cfb34ccfbf48cbdcf8de5aef2857ce362875
jasleenwahi <jasleen.wahi@knoldus.com>
2023-10-23 15:41:45 +0530

    asserting response time for the api tests

--- go1percent/test/contribution_edit_api.js
+++ go1percent/test/contribution_edit_api.js
@@ -4,6 +4,7 @@ describe('api testing', function () {
     //This test sends a GET request to retrieve details of a specific contribution using the contribution ID.
 
     it('click on a certain contribution and see the details', async function({supertest}) {
+      const startTime = new Date().getTime();
       await supertest
         .request(headers.baseurl)
         .get("/contribution?contributionId=2729")
@@ -21,10 +22,17 @@ describe('api testing', function () {
             expect(response.body.data).to.have.property('status');
 
         });
+
+      const endTime = new Date().getTime();
+      const responseTime = endTime - startTime;
+      //asserting response time.
+      expect(responseTime).to.be.lessThan(2000);
+
     });
 
     //This test sends a GET request to retrieve knolder contributions with pagination and a limit.
     it('on saving changes for a contribution', async function({supertest}) {
+        const startTime = new Date().getTime();
         await supertest
 
           .request(headers.baseurl)
@@ -37,6 +45,7 @@ describe('api testing', function () {
 
             //The test then checks the response body for the presence of specific properties for the first contribution, such as "contributionId," "knolderName," "title," "status," and "contributionType."
             expect(Object.keys(response.body).length).to.be.greaterThan(0);
+            expect(response.body.data._1).length.to.be.greaterThan(0);
             expect(response.body.data._1[0]).to.have.property('contributionId');
             expect(response.body.data._1[0]).to.have.property('knolderName');
             expect(response.body.data._1[0]).to.have.property('title');
@@ -44,16 +53,23 @@ describe('api testing', function () {
             expect(response.body.data._1[0]).to.have.property('contributionType');
 
         });
+
+        const endTime = new Date().getTime();
+        const responseTime = endTime - startTime;
+        //asserting response time.
+        expect(responseTime).to.be.lessThan(2000);
           
       });
 
       //This test sends a PUT request to edit a contribution with updated data.
       it('on editing a contribution', async function({supertest}) {
+        const startTime = new Date().getTime();
+
         await supertest
           .request("https://backend.qa.go1percent.com")
           .put('/contribution/editContribution')
           .set(headers.headers)
-          .send({"title":"fbgdhgnbgfndgnngfjdgj","contributionType":"Conference","contributionDate":"2023-10-11 00:00:00","urlDetails":"www.cou.org","technologyDetails":"dsfnwejkfewhfuiehfioejwfoijcoiwndoweifnencvdvejdsncdjdsklwedjknhcfudsiefewhrtoihjfoivdhsjnvdjsnfvdkjsnvkjdsnvkj","contributionId":2729})
+          .send({"title":"Test employee contribution","contributionType":"Research paper","contributionDate":"2023-10-19 00:00:00","urlDetails":"www.courser.org","technologyDetails":"ddcscdcmjjicjdjc hdfiwic jocdjoicwd fcdiocd chojvwofeefhevdvhvhnvjvjdwehuihefkjcnjkvnwbrihfuvbhjbdjfre","contributionId":2727})
           
           .expect(200)
           .expect('Content-Type', /json/)
@@ -67,6 +83,11 @@ describe('api testing', function () {
             expect(response.body.data).to.be.equal('contribution Updated Successfully!')
             
         });
+
+        const endTime = new Date().getTime();
+        const responseTime = endTime - startTime;
+        //asserting response time.
+        expect(responseTime).to.be.lessThan(2000);
           
       });
 
