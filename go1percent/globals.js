f694a53d265a9c4edf7552e2e616f3c401d4489f
Nashjyoti <jyoti.chauhan@nashtechglobal.com>
2023-10-25 16:12:26 +0530

    Modified gitignore

--- go1percent/globals.js
+++ go1percent/globals.js
@@ -11,19 +11,7 @@ module.exports = {
         chromedriver.stop();
         done();
     },
-    headers:
-    {
-    
-                'sec-ch-ua': '"Chromium";v="118", "Google Chrome";v="118", "Not=A?Brand";v="99"',
-                'LanguageValue': 'en',
-                'Source':'https://nashtechglobal.qa.go1percent.com',
-                'Authorization': '',
-                'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36',
-                'Accept':'application/json, text/plain, */*',
-                'sec-ch-ua-platform':'Linux',
-                'host': 'backend.qa.go1percent.com'
-              
-        },
+   
 
     Url:
     {
