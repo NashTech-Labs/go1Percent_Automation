220b3b367b1bed50d408b67bbe95875a57f87a19
vanshika1221 <vanshika.srivastava@knoldus.com>
2023-10-25 18:47:19 +0530

    Added more api test cases in Rewards Section

--- go1percent/globals.js
+++ go1percent/globals.js
@@ -10,26 +10,6 @@ module.exports = {
         done();
     },
 
-headers: {
-        
-    "authority": "backend.qa.go1percent.com",
-    "accept": "application/json, text/plain, */*",
-    "accept-language": "en-US,en;q=0.9",
-    "authorization": "Bearer eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJIRzk4ejl1eFN0T3VPaVg4a2RiYlNPblQ2a29OWUlCSENLeXM2LUhTYnlFIn0.eyJleHAiOjE2OTgyMzE2ODYsImlhdCI6MTY5ODIyOTg4NiwiYXV0aF90aW1lIjoxNjk4MjI5ODg1LCJqdGkiOiJmMzdiZWI4Yi0yZTFhLTQ0ZmUtYTM2OC02MTMxYTY4OGEwNDkiLCJpc3MiOiJodHRwczovL2F1dGguZ28xcGVyY2VudC5jb20vYXV0aC9yZWFsbXMvbmFzaHRlY2giLCJhdWQiOlsibGVhZGVyYm9hcmQtdWkiLCJsZWFkZXJib2FyZC1kZXYtdWkiLCJhY2NvdW50Il0sInN1YiI6ImE3MTlhMmIyLTNjYWMtNGMxMi05NDViLWQ3MzMwYTgxOTEwNiIsInR5cCI6IkJlYXJlciIsImF6cCI6ImxlYWRlcmJvYXJkLXFhLXVpIiwibm9uY2UiOiJlNGE1ZGZmOS0yYzc1LTQ1ZmUtYTVjZi0yMTU5MmJlZWNhOGIiLCJzZXNzaW9uX3N0YXRlIjoiYzVlNWZhNTQtZjA4NC00N2Y2LWFkYzUtNzQ5MTQwNmQ5ZGMyIiwiYWNyIjoiMSIsImFsbG93ZWQtb3JpZ2lucyI6WyJodHRwczovL25hc2h0ZWNoZ2xvYmFsLnFhLmdvMXBlcmNlbnQuY29tIiwiaHR0cHM6Ly9sZWFkZXJib2FyZC1xYS5rbm9sZHVzLmNvbSIsImh0dHBzOi8va25vbGR1cy5xYS5nbzFwZXJjZW50LmNvbSIsImh0dHBzOi8vcmVwb3J0LnFhLmdvMXBlcmNlbnQuY29tIiwiaHR0cHM6Ly9sZWFkZXJib2FyZC1xYS5nbzFwZXJjZW50LmNvbSIsImh0dHA6Ly9sb2NhbGhvc3Q6ODA4OCIsImh0dHA6Ly9sb2NhbGhvc3Q6NDIwMCJdLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsib2ZmbGluZV9hY2Nlc3MiLCJkZWZhdWx0LXJvbGVzLW5hc2h0ZWNoIiwidW1hX2F1dGhvcml6YXRpb24iLCJlbXBsb3llZSJdfSwicmVzb3VyY2VfYWNjZXNzIjp7ImxlYWRlcmJvYXJkLXFhLXVpIjp7InJvbGVzIjpbImFkbWluIiwiZW1wbG95ZWUiXX0sImxlYWRlcmJvYXJkLXVpIjp7InJvbGVzIjpbImFkbWluIiwiZW1wbG95ZWUiXX0sImxlYWRlcmJvYXJkLWRldi11aSI6eyJyb2xlcyI6WyJhZG1pbiIsImVtcGxveWVlIl19LCJhY2NvdW50Ijp7InJvbGVzIjpbIm1hbmFnZS1hY2NvdW50IiwibWFuYWdlLWFjY291bnQtbGlua3MiLCJ2aWV3LXByb2ZpbGUiXX19LCJzY29wZSI6Im9wZW5pZCBlbWFpbCBwcm9maWxlIiwic2lkIjoiYzVlNWZhNTQtZjA4NC00N2Y2LWFkYzUtNzQ5MTQwNmQ5ZGMyIiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJuYW1lIjoidGVzdCBhZG1pbiIsInByZWZlcnJlZF91c2VybmFtZSI6InRlc3RhZG1pbiIsImdpdmVuX25hbWUiOiJ0ZXN0IiwiZmFtaWx5X25hbWUiOiJhZG1pbiIsImVtYWlsIjoidGVzdGFkbWluQG5hc2h0ZWNoZ2xvYmFsLmNvbSJ9.WY4vw4WOOSueBpEfIw1X-_hyxBDOfvNDmHg7EPgbybi8af0p1vO_qdDliUfOOSc5hT4FbfFKSEPtlmAE6IPn_SbZkcNHLqV7IL7CXLS_U0GdyF235Hcl5SOIABS8XT411YZRNhRUfAIMJbKXt7F7Z8MyLHJeVCz8kYs_U5N4j-KdjsTAJs9CBQ8Nf35GDswQ8ktunQXkA565Q4Yyl-m4tpMhycfeJPbYVwjYfFE5UhFI9gM0R6EfE0eIrHpfKHe8Tr0mh6PpnXrQgsN9-uzU33HhH_AnHsi-JPB4uY51cBnTT4n9_I3SNHyp2aQEd4tmSFhCqdTwupyxRbBb9YhR-w",
-    "languagevalue": "en",
-    "origin": "https://nashtechglobal.qa.go1percent.com",
-    "referer": "https://nashtechglobal.qa.go1percent.com/",
-    "sec-ch-ua": "\"Chromium\";v=\"116\", \"Not)A;Brand\";v=\"24\", \"Google Chrome\";v=\"116\"",
-    "sec-ch-ua-mobile": "?0",
-    "sec-ch-ua-platform": "\"Linux\"",
-    "sec-fetch-dest": "empty",
-    "sec-fetch-mode": "cors",
-    "sec-fetch-site": "same-site",
-    "source": "https://nashtechglobal.qa.go1percent.com",
-    "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"
-  
-  
-}
 };
 
 
