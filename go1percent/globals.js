3c2ce76198760c75c6078096ed11bb95f14aad35
mansijain1knoldus <mansi.jain1@knoldus.com>
2023-10-26 18:44:19 +0530

    Changes updated

--- go1percent/globals.js
+++ go1percent/globals.js
@@ -1,5 +1,9 @@
 const chromedriver = require('chromedriver');
 
+function startTime(){
+    return new Date().getTime()
+}
+
 module.exports = {
 
     before: function(done) {
@@ -10,6 +14,16 @@ module.exports = {
     after: function(done) {
         chromedriver.stop();
         done();
-    }
+    },
 
+    queryRequested:{
+        'pageNumber' : '1',
+        'filter' : 'requested', 
+        'pageSize' : '10',
+        'search' : '',
+    },
+
+    base_url : "https://knolx-backend.qa.go1percent.com/",
+    source : "https://nashtechglobal.qa.go1percent.com",
+    access_token : '',
 };
