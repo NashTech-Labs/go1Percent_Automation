e975761eb9ccf9fbc38053409a06340f9ef7cf27
tarunrawatknoldus <tarun.rawat@knoldus.com>
2023-10-26 15:14:20 +0530

    new APi updates

--- go1percent/globals.js
+++ go1percent/globals.js
@@ -8,10 +8,6 @@ function startTime(){
     return new Date().getTime()
 }
 
-function startTime(){
-    return new Date().getTime()
-}
-
 module.exports = {
 
     before: function(done) {
@@ -22,7 +18,6 @@ module.exports = {
     after: function(done) {
         chromedriver.stop();
         done();
-<<<<<<< HEAD
     },      
     // Upcoming Page
     queryNasher: {
@@ -87,17 +82,6 @@ module.exports = {
     access_token :"",
     url : 'https://backend-radar.qa.go1percent.com/',
 
-    baseurl:'https://backend.qa.go1percent.com',
-    base_url : "https://knolx-backend.qa.go1percent.com",
-    source : "https://nashtechglobal.qa.go1percent.com",
-=======
-    },
-    
-    baseURL : 'https://knolx-backend.qa.go1percent.com', // Base URL of the API
-    accessToken : 'Bearer <Your Access Token>', // Your access token here
-    source: 'https://nashtechglobal.qa.go1percent.com', 
-
->>>>>>> 37ad7bbcbcc721b6831b5651aa4f9ac23cc0ed6b
     dataToUpdate: {
         sessionTag: [],
         sessionDescription: "The error message indicates that the property 'topic' is not present in the response body, which is why the assertion is failing. This could be due to the structure of the response body or the way the API is handling the request. Please ensure that the API response structure matches the expected response format.",
