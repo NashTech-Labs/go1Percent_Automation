ce1b347a09ea4eb61e64ffa10523daa59b57ce29
RahulKnolder <rahul.sinha@knolder.com>
2023-10-26 15:46:15 +0530

    Added test cases for feature/leaderboardsummary

--- go1percent/globals.js
+++ go1percent/globals.js
@@ -2,52 +2,34 @@
 
 module.exports = {
 
-    before: function(done) {
+    before: function (done) {
         chromedriver.start();
         done();
     },
 
-    after: function(done) {
+    after: function (done) {
         chromedriver.stop();
         done();
     },
-    // headers:{
-    //     'authority': 'backend.qa.go1percent.com',
-    //     'accept': 'application/json, text/plain, */*',
-    //     'accept-language': 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJIRzk4ejl1eFN0T3VPaVg4a2RiYlNPblQ2a29OWUlCSENLeXM2LUhTYnlFIn0.eyJleHAiOjE2OTc4MjAyNjQsImlhdCI6MTY5NzgxODQ2NCwiYXV0aF90aW1lIjoxNjk3ODE4NDYzLCJqdGkiOiI4ZjJmYmViYy05NmEwLTQ2NjEtOWMyZS1jODc3NGI5YmY1NTkiLCJpc3MiOiJodHRwczovL2F1dGguZ28xcGVyY2VudC5jb20vYXV0aC9yZWFsbXMvbmFzaHRlY2giLCJhdWQiOlsibGVhZGVyYm9hcmQtdWkiLCJsZWFkZXJib2FyZC1kZXYtdWkiLCJhY2NvdW50Il0sInN1YiI6IjgzYjAzNGY1LTg2NzUtNGQ5NC04NDQ0LTg3ZjZlYTk4YzlhYiIsInR5cCI6IkJlYXJlciIsImF6cCI6ImxlYWRlcmJvYXJkLXFhLXVpIiwibm9uY2UiOiIxMDk1YWY1YS04NjgzLTQ2MTMtODg3Mi1hNDU0OGJkMGMwYTgiLCJzZXNzaW9uX3N0YXRlIjoiZDFmY2VlZTAtZWE2Zi00ZTRjLWI4MTMtYzMyODg5ODlmMTgyIiwiYWNyIjoiMSIsImFsbG93ZWQtb3JpZ2lucyI6WyJodHRwczovL25hc2h0ZWNoZ2xvYmFsLnFhLmdvMXBlcmNlbnQuY29tIiwiaHR0cHM6Ly9sZWFkZXJib2FyZC1xYS5rbm9sZHVzLmNvbSIsImh0dHBzOi8va25vbGR1cy5xYS5nbzFwZXJjZW50LmNvbSIsImh0dHBzOi8vcmVwb3J0LnFhLmdvMXBlcmNlbnQuY29tIiwiaHR0cHM6Ly9sZWFkZXJib2FyZC1xYS5nbzFwZXJjZW50LmNvbSIsImh0dHA6Ly9sb2NhbGhvc3Q6ODA4OCIsImh0dHA6Ly9sb2NhbGhvc3Q6NDIwMCJdLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsib2ZmbGluZV9hY2Nlc3MiLCJkZWZhdWx0LXJvbGVzLW5hc2h0ZWNoIiwidW1hX2F1dGhvcml6YXRpb24iLCJlbXBsb3llZSJdfSwicmVzb3VyY2VfYWNjZXNzIjp7ImxlYWRlcmJvYXJkLXFhLXVpIjp7InJvbGVzIjpbImVtcGxveWVlIl19LCJsZWFkZXJib2FyZC11aSI6eyJyb2xlcyI6WyJlbXBsb3llZSJdfSwibGVhZGVyYm9hcmQtZGV2LXVpIjp7InJvbGVzIjpbImVtcGxveWVlIl19LCJhY2NvdW50Ijp7InJvbGVzIjpbIm1hbmFnZS1hY2NvdW50IiwibWFuYWdlLWFjY291bnQtbGlua3MiLCJ2aWV3LXByb2ZpbGUiXX19LCJzY29wZSI6Im9wZW5pZCBlbWFpbCBwcm9maWxlIiwic2lkIjoiZDFmY2VlZTAtZWE2Zi00ZTRjLWI4MTMtYzMyODg5ODlmMTgyIiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJuYW1lIjoidGVzdCBlbXBsb3llZSIsInByZWZlcnJlZF91c2VybmFtZSI6InRlc3RlbXBsb3llZSIsImdpdmVuX25hbWUiOiJ0ZXN0IiwiZmFtaWx5X25hbWUiOiJlbXBsb3llZSIsImVtYWlsIjoidGVzdGVtcGxveWVlQG5hc2h0ZWNoZ2xvYmFsLmNvbSJ9.byGZr8XkgdbGUS7PItuApsldleAG3cdppRJWveL4jO8XVpExSA-uOAhcPfGZr2PEOv5ZJLHtMVJe8bpiC1Nb90yx1WfwmZBGIQHKf7yJUurngmWxtncwzPZHZOFmnrXldHO5TLs_SHEAwriz4QPHefTQV57msEOyLSQpVTn7O_rE4ZaCzh7AvYhS0_M7fpJt9saxdQ3ktyjAnv393T595In9jdsNcaHGwLBlAjTMt3FPiJ_ToMpTk7EzNyjpKFRsRQ3MqrT2AlNqoayoSmtwgBsQZp7GTzavwmtU0GFqsppbSbLxpjTY2k0cU_Blz7obs3Xfy5ujFWvwbMYefGgC2Q',
-    //     'languagevalue': 'en',
-    //     'origin': 'https://nashtechglobal.qa.go1percent.com',
-    //     'referer': 'https://nashtechglobal.qa.go1percent.com/',
-    //     'sec-ch-ua': '"Chromium";v="116", "Not)A;Brand";v="24", "Google Chrome";v="116"',
-    //     'sec-ch-ua-mobile': '?0',
-    //     'sec-ch-ua-platform': '"Linux"',
-    //     'sec-fetch-dest': 'empty',
-    //     'sec-fetch-mode': 'cors',
-    //     'sec-fetch-site': 'same-site',
-    //     'source': 'https://nashtechglobal.qa.go1percent.com',
-    //     'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
-    //   }
-      headers:{
+    headers: {
         
-            "authority": "backend.qa.go1percent.com",
-            "accept": "application/json, text/plain, */*",
-            "accept-language": "en-US,en;q=0.9",
-            "authorization": "Bearer eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJIRzk4ejl1eFN0T3VPaVg4a2RiYlNPblQ2a29OWUlCSENLeXM2LUhTYnlFIn0.eyJleHAiOjE2OTc4MjU5NTMsImlhdCI6MTY5NzgyNDE1MywiYXV0aF90aW1lIjoxNjk3ODI0MTUyLCJqdGkiOiIxMjRjNDhhYy1jNWI2LTQ5YTItYTcwNi1lYmM3MWRmZWQ5ODQiLCJpc3MiOiJodHRwczovL2F1dGguZ28xcGVyY2VudC5jb20vYXV0aC9yZWFsbXMvbmFzaHRlY2giLCJhdWQiOlsibGVhZGVyYm9hcmQtdWkiLCJsZWFkZXJib2FyZC1kZXYtdWkiLCJhY2NvdW50Il0sInN1YiI6IjgzYjAzNGY1LTg2NzUtNGQ5NC04NDQ0LTg3ZjZlYTk4YzlhYiIsInR5cCI6IkJlYXJlciIsImF6cCI6ImxlYWRlcmJvYXJkLXFhLXVpIiwibm9uY2UiOiJjMmY0OTZiYy05NjM5LTRhMDctOTY4MS05YjNjOGIzY2IzMDAiLCJzZXNzaW9uX3N0YXRlIjoiYjYwYzk0OTAtNzMwYi00Y2Q4LWJiZjktMjk1ZjQ3M2FkMDk1IiwiYWNyIjoiMSIsImFsbG93ZWQtb3JpZ2lucyI6WyJodHRwczovL25hc2h0ZWNoZ2xvYmFsLnFhLmdvMXBlcmNlbnQuY29tIiwiaHR0cHM6Ly9sZWFkZXJib2FyZC1xYS5rbm9sZHVzLmNvbSIsImh0dHBzOi8va25vbGR1cy5xYS5nbzFwZXJjZW50LmNvbSIsImh0dHBzOi8vcmVwb3J0LnFhLmdvMXBlcmNlbnQuY29tIiwiaHR0cHM6Ly9sZWFkZXJib2FyZC1xYS5nbzFwZXJjZW50LmNvbSIsImh0dHA6Ly9sb2NhbGhvc3Q6ODA4OCIsImh0dHA6Ly9sb2NhbGhvc3Q6NDIwMCJdLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsib2ZmbGluZV9hY2Nlc3MiLCJkZWZhdWx0LXJvbGVzLW5hc2h0ZWNoIiwidW1hX2F1dGhvcml6YXRpb24iLCJlbXBsb3llZSJdfSwicmVzb3VyY2VfYWNjZXNzIjp7ImxlYWRlcmJvYXJkLXFhLXVpIjp7InJvbGVzIjpbImVtcGxveWVlIl19LCJsZWFkZXJib2FyZC11aSI6eyJyb2xlcyI6WyJlbXBsb3llZSJdfSwibGVhZGVyYm9hcmQtZGV2LXVpIjp7InJvbGVzIjpbImVtcGxveWVlIl19LCJhY2NvdW50Ijp7InJvbGVzIjpbIm1hbmFnZS1hY2NvdW50IiwibWFuYWdlLWFjY291bnQtbGlua3MiLCJ2aWV3LXByb2ZpbGUiXX19LCJzY29wZSI6Im9wZW5pZCBlbWFpbCBwcm9maWxlIiwic2lkIjoiYjYwYzk0OTAtNzMwYi00Y2Q4LWJiZjktMjk1ZjQ3M2FkMDk1IiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJuYW1lIjoidGVzdCBlbXBsb3llZSIsInByZWZlcnJlZF91c2VybmFtZSI6InRlc3RlbXBsb3llZSIsImdpdmVuX25hbWUiOiJ0ZXN0IiwiZmFtaWx5X25hbWUiOiJlbXBsb3llZSIsImVtYWlsIjoidGVzdGVtcGxveWVlQG5hc2h0ZWNoZ2xvYmFsLmNvbSJ9.HmMrpZhta4s2kcAi3POQuxScDOF45gXCoPvARLe3tID3pq9UpnY1RLAHNEnEIUtG2dp7hYRh4_MtJVC71ssEy6TzppLL1CHQrU3lnc5hXGk0P26MtGwhZMWlxSS80lvQ7edjGf0hf7kGygr7esehhVb_BuWZhydimXdlywcVL91mo5736kF4AHCQr3n6r1nRYtPhUNQe_ZsOobvjddqF1nHWbdoZVrgU0cMwxVVXkueZ5OvLYi-EDJD9I3-S5FE-lAS_e4IKhK4wX5nAROVDC7SGq9ZWxkwphxE8vh9pDGHzp0oG8voLldmsFk-moMlAW5iE38zc3U9MTXcEyZdlAQ",
-            "languagevalue": "en",
-            "origin": "https://nashtechglobal.qa.go1percent.com",
-            "referer": "https://nashtechglobal.qa.go1percent.com/",
-            "sec-ch-ua": "\"Chromium\";v=\"116\", \"Not)A;Brand\";v=\"24\", \"Google Chrome\";v=\"116\"",
-            "sec-ch-ua-mobile": "?0",
-            "sec-ch-ua-platform": "\"Linux\"",
-            "sec-fetch-dest": "empty",
-            "sec-fetch-mode": "cors",
-            "sec-fetch-site": "same-site",
-            "source": "https://nashtechglobal.qa.go1percent.com",
-            "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"
+            "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/118.0",
+            "Accept": "application/json, text/plain, */*",
+            "Accept-Language": "en-US,en;q=0.5",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Authorization": "Bearer eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJIRzk4ejl1eFN0T3VPaVg4a2RiYlNPblQ2a29OWUlCSENLeXM2LUhTYnlFIn0.eyJleHAiOjE2OTgzMTUwOTAsImlhdCI6MTY5ODMxMzI5MCwiYXV0aF90aW1lIjoxNjk4MzEzMjg4LCJqdGkiOiIzNDJmNWY5Mi00YTJlLTQ1ZTgtOGFiNS02ZWYzZDIyOGE4MGIiLCJpc3MiOiJodHRwczovL2F1dGguZ28xcGVyY2VudC5jb20vYXV0aC9yZWFsbXMvbmFzaHRlY2giLCJhdWQiOlsibGVhZGVyYm9hcmQtdWkiLCJsZWFkZXJib2FyZC1kZXYtdWkiLCJhY2NvdW50Il0sInN1YiI6IjgzYjAzNGY1LTg2NzUtNGQ5NC04NDQ0LTg3ZjZlYTk4YzlhYiIsInR5cCI6IkJlYXJlciIsImF6cCI6ImxlYWRlcmJvYXJkLXFhLXVpIiwibm9uY2UiOiIxZjg4MThlZC04NmI2LTQzMzctOTg2OC04YzU0MDAxYjViMGYiLCJzZXNzaW9uX3N0YXRlIjoiYzc2ZTdiZTgtNTY4OS00MmQzLWE3NWMtMmM4ODkxOTVjNzQ4IiwiYWNyIjoiMSIsImFsbG93ZWQtb3JpZ2lucyI6WyJodHRwczovL25hc2h0ZWNoZ2xvYmFsLnFhLmdvMXBlcmNlbnQuY29tIiwiaHR0cHM6Ly9sZWFkZXJib2FyZC1xYS5rbm9sZHVzLmNvbSIsImh0dHBzOi8va25vbGR1cy5xYS5nbzFwZXJjZW50LmNvbSIsImh0dHBzOi8vcmVwb3J0LnFhLmdvMXBlcmNlbnQuY29tIiwiaHR0cHM6Ly9sZWFkZXJib2FyZC1xYS5nbzFwZXJjZW50LmNvbSIsImh0dHA6Ly9sb2NhbGhvc3Q6ODA4OCIsImh0dHA6Ly9sb2NhbGhvc3Q6NDIwMCJdLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsib2ZmbGluZV9hY2Nlc3MiLCJkZWZhdWx0LXJvbGVzLW5hc2h0ZWNoIiwidW1hX2F1dGhvcml6YXRpb24iLCJlbXBsb3llZSJdfSwicmVzb3VyY2VfYWNjZXNzIjp7ImxlYWRlcmJvYXJkLXFhLXVpIjp7InJvbGVzIjpbImVtcGxveWVlIl19LCJsZWFkZXJib2FyZC11aSI6eyJyb2xlcyI6WyJlbXBsb3llZSJdfSwibGVhZGVyYm9hcmQtZGV2LXVpIjp7InJvbGVzIjpbImVtcGxveWVlIl19LCJhY2NvdW50Ijp7InJvbGVzIjpbIm1hbmFnZS1hY2NvdW50IiwibWFuYWdlLWFjY291bnQtbGlua3MiLCJ2aWV3LXByb2ZpbGUiXX19LCJzY29wZSI6Im9wZW5pZCBlbWFpbCBwcm9maWxlIiwic2lkIjoiYzc2ZTdiZTgtNTY4OS00MmQzLWE3NWMtMmM4ODkxOTVjNzQ4IiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJuYW1lIjoidGVzdCBlbXBsb3llZSIsInByZWZlcnJlZF91c2VybmFtZSI6InRlc3RlbXBsb3llZSIsImdpdmVuX25hbWUiOiJ0ZXN0IiwiZmFtaWx5X25hbWUiOiJlbXBsb3llZSIsImVtYWlsIjoidGVzdGVtcGxveWVlQG5hc2h0ZWNoZ2xvYmFsLmNvbSJ9.P7DpDUe-3pxK_QQlO2Yni6OK9b16JSITQ4m1ZN64PWpSEH_0b6-B77kWcd_HY4NMU6aiqL-ZgMRnCAw4KPGUVWo_yUDOMwyfMeOP0CUKtpk2jsbF5sFSCxmvlSOVNTrZBSh6AmDHRrmEHS5cZmqh9t_t1tMC1H02gXPXuIHu9sRU7xihfbIQEhTa5D5CgwN6PbQpO5FOesnW1P8_c5Ku4oJ8gvtgt_A7bhS58gxVyMvUvR7um2jO9tC0jdMrKvYs0a04MX-fr0JQJA71Cr1o6pzRt-cMghCNaQ19o25Jtrkt3xxW2k9NJmSACf2wEZJica8GyVycwpjuEyrAJHFsrA",
+            "LanguageValue": "en",
+            "Source": "https://nashtechglobal.qa.go1percent.com",
+            "Origin": "https://nashtechglobal.qa.go1percent.com",
+            "Connection": "keep-alive",
+            "Referer": "https://nashtechglobal.qa.go1percent.com/",
+            "Sec-Fetch-Dest": "empty",
+            "Sec-Fetch-Mode": "cors",
+            "Sec-Fetch-Site": "same-site",
+            "TE": "trailers"          
           
-          
-      }
-      
+    }
+
 
 
 };
--- go1percent/test/api-testing/leaderboardSummaryApiTest.js
+++ go1percent/test/api-testing/leaderboardSummaryApiTest.js
@@ -10,14 +10,21 @@ describe('Leaderboard API Testing', function () {
    * @param {Object} supertest - The supertest object for making HTTP requests.
    */
   it('should retrieve Leading Practices Summary', async function ({ supertest }) {
+    const startTime = performance.now();
+
     const response = await supertest
       .request(baseUrl)
       .get('/studios')
       .set(headers)
       .expect(200) // Expect a successful response with HTTP status code 200
       .expect('Content-Type', /json/); // Expect the response to have JSON content type
+    const endTime = performance.now();
+
+    // Assert that the execution time is less than or equal to 1500 milliseconds.
+    expect(endTime - startTime).to.be.at.most(1500);
 
     const responseBody = response.body;
+
     expect(responseBody.length).to.be.greaterThan(0);
 
     for (let indexOfResponse = 0; indexOfResponse < responseBody.length; indexOfResponse++) {
@@ -61,17 +68,11 @@ describe('Leaderboard API Testing', function () {
       expect(object.allTimeScore).to.be.a('number');
       expect(object.studioHeadEmail).to.be.a('string');
       expect(object.monthlyScore).to.be.a('number');
-      expect(object.memberAvg).to.be.a('number'); 
+      expect(object.memberAvg).to.be.a('number');
       expect(object.memberAllTimeAvg).to.be.a('number');
       expect(object.monthlyScore).to.be.a('number');
-      expect(object.memberAvg).to.be.a('number'); 
-      expect(object.memberCount).to.be.a('number'); 
-
-      if (object.studioHeadName === null) {
-        expect(object.studioHeadName).to.be.null;
-      } else {
-        expect(object.studioHeadName).to.be.a('string');
-      }
+      expect(object.memberAvg).to.be.a('number');
+      expect(object.memberCount).to.be.a('number');
     }
   });
 
@@ -81,12 +82,19 @@ describe('Leaderboard API Testing', function () {
    * @param {Object} supertest - The supertest object for making HTTP requests.
    */
   it('should retrieve Leading Nasher data for all times', async function ({ supertest }) {
+    const startTime = performance.now();
+
     const response = await supertest
       .request(baseUrl)
       .get('/summary?period=alltime')
       .set(headers)
       .expect(200) // Expect a successful response with HTTP status code 200
       .expect('Content-Type', /json/); // Expect the response to have JSON content type
+
+    const endTime = performance.now();
+
+    // Assert that the execution time is less than or equal to 1500 milliseconds.
+    expect(endTime - startTime).to.be.at.most(1500);
 
     // Assert that the response body has data
     expect(response.body.length).to.be.greaterThan(0);
@@ -114,6 +122,316 @@ describe('Leaderboard API Testing', function () {
       expect(object.score).to.be.a('number');
       expect(object.rank).to.be.a('number');
     }
+  });
+
+
+  /**
+  * Test case to retrieve Leading Nasher data for all times.
+  * @param {Object} supertest - The supertest object for making HTTP requests.
+  */
+  it('should retrieve Leading Nasher data monthly', async function ({ supertest }) {
+    const startTime = performance.now();
+
+    const response = await supertest
+      .request(baseUrl)
+      .get('/summary?period=monthly')
+      .set(headers)
+      .expect(200) 
+      .expect('Content-Type', /json/); // Expect the response to have JSON content type
+
+    const endTime = performance.now();
+
+    // Assert that the execution time is less than or equal to 1500 milliseconds.
+    expect(endTime - startTime).to.be.at.most(1500);
+
+    // Assert that the response body has data
+    expect(response.body.length).to.be.greaterThan(0);
+
+    const responseBody = response.body;
+
+    for (let indexOfResponse = 0; indexOfResponse < responseBody.length; indexOfResponse++) {
+      const object = responseBody[indexOfResponse];
+
+      // Check if the object has the expected keys
+      expect(object).to.have.all.keys(
+        'knolderId',
+        'KnolderName',
+        'profilePic',
+        'studioName',
+        'score',
+        'rank'
+      );
+
+      // Assert the types of each property's value
+      expect(object.knolderId).to.be.a('number');
+      expect(object.KnolderName).to.be.a('string');
+      expect(object.profilePic).to.be.a('string');
+      expect(object.studioName).to.be.a('string');
+      expect(object.score).to.be.a('number');
+      expect(object.rank).to.be.a('number');
+    }
+  });
+
+
+  /**
+* Test case to retrieve monthly data for Leading Nasher users, including counts of Certifications,
+* Online Courses, Meetups, Proposals, Process Docs, Techhub, Conferences, Research Papers,
+* Books, Blogs, Knolx, Webinars, OS Contributions, and reputation.
+* @param {Object} supertest - The supertest object for making HTTP requests.
+*/
+  it('should retrieve Leading Nasher data', async function ({ supertest }) {
+
+    const startTime = performance.now();
+
+    const response = await supertest
+      .request(baseUrl)
+      .get('/reputation')
+      .set(headers)
+      .expect(200)
+      .expect('Content-Type', /json/);
+
+    const endTime = performance.now();
+
+    // Assert that the execution time is less than or equal to 1500 milliseconds.
+    expect(endTime - startTime).to.be.at.most(1500);
+
+    // Check if the response contains "counts" and "reputation" properties
+    expect(response.body).to.have.all.keys('counts', 'reputation');
+
+    // Check the "counts" property
+    const counts = response.body.counts;
+    for (const key in counts) {
+      expect(counts[key]).to.have.all.keys('monthly', 'allTime');
+    }
+
+    // Check the "reputation" property
+    const reputation = response.body.reputation;
+    for (const object of reputation) {
+      expect(object).to.have.all.keys(
+        'knolderId',
+        'knolderName',
+        'studioId',
+        'studioName',
+        'allTimeScore',
+        'allTimeRank',
+        'quarterlyStreak',
+        'monthlyScore',
+        'monthlyRank',
+        'rewardPoints'
+      );
+    }
+  });
+
+
+
+  /**
+ * Test case to retrieve  data for Leading Nasher users, including counts of Certifications,
+ * Online Courses, Meetups, Proposals, Process Docs, Techhub, Conferences, Research Papers,
+ * Books, Blogs, Knolx, Webinars, OS Contributions, and reputation in a perticular month .
+ * @param {Object} supertest - The supertest object for making HTTP requests.
+ */
+  it('should retrieve Leading Nasher of specific month', async function ({ supertest }) {
+    const startTime = performance.now();
+
+    const response = await supertest
+      .request(baseUrl)
+      .get('/reputation/666?month=October&year=2023')
+      .set(headers)
+      .expect(200)
+      .expect('Content-Type', /json/);
+
+    const endTime = performance.now();
+
+    // Assert that the execution time is less than or equal to 1000 milliseconds.
+    expect(endTime - startTime).to.be.at.most(1000);
+    // Check if the response contains "counts" and "reputation" properties
+    expect(response.body).to.have.all.keys(
+      'knolderEmail',
+      'knolderName',
+      'score',
+      'scoreBreakDown',
+      'studioId',
+      'studioName');
+  });
+
+
+  /**
+   * Test case to retrieve all activities of a user's profile.
+   * This test case makes a GET request to retrieve all activities of a specific user's profile.
+   * @param {Object} supertest - The supertest object for making HTTP requests.
+   */
+  it('should retrieve user profile activities', async function ({ supertest }) {
+
+    const startTime = performance.now();
+    // Make a GET request to retrieve user profile activities
+    const response = await supertest
+      .request('https://feed-service.qa.go1percent.com/')
+      .get('/knoldus-backend/rest/feed-service/feeds/userProfile/myActivity/0/testemployee@nashtechglobal.com')
+      .expect(200) // Expect a successful response with HTTP status code 200
+      .expect('Content-Type', /json/);
+    const endTime = performance.now();
+    const responseBody = response.body;
+
+    // Assert that the execution time is less than or equal to 1000 milliseconds.
+    expect(endTime - startTime).to.be.at.most(1000);
+
+    for (let indexOfResponse = 0; indexOfResponse < responseBody.length; indexOfResponse++) {
+      const object = responseBody[indexOfResponse];
+
+      // Check if the object has the expected keys
+      expect(object).to.have.all.keys(
+        'feedType',
+        'username',
+        'likes',
+        'dislike',
+        'contributedBy',
+        'contributionType',
+        'description',
+        'feedcomment',
+        'title',
+        'feedtime',
+        'lapseTimeDifference'
+      );
+
+      // Assert the types of each property's value
+      expect(object.feedType).to.be.a('string');
+      expect(object.username).to.be.a('string');
+      expect(object.likes).to.satisfy(value => typeof value === 'string' || value === null);
+      expect(object.contributedBy).to.be.a('string');
+      expect(object.contributionType).to.be.a('string');
+      expect(object.description).to.be.a('string');
+      expect(object.feedcomment).to.satisfy(value => typeof value === 'string' || value === null);
+      expect(object.title).to.be.a('string');
+      expect(object.feedtime).to.be.a('number');
+      expect(object.lapseTimeDifference).to.be.a('string');
+    }
+  });
+
+
+  /**
+   * Test case for handling the scenario when the profile picture is not found.
+   * Sends a GET request to retrieve a user's profile picture and expects an error response.
+   * @param {Object} supertest - The supertest object for making HTTP requests.
+   */
+
+  it('should handle profile picture not found', async function ({ supertest }) {
+
+    // Record the start time before making the request
+    const startTime = performance.now();
+
+    const response = await supertest
+      .request(baseUrl)
+      .get('/profile/getProfilePic?email=testemployee@nashtechglobal.com')
+      .set(headers)
+      .expect('Content-Type', /json/); // Expect the specific content type
+
+    // Record the end time after receiving the response
+
+    const endTime = performance.now();
+    const responseBody = response.body;
+
+    // Assert that the response time is within an expected range, e.g., less than or equal to 1000 milliseconds
+    expect(endTime - startTime).to.be.at.most(1000);
+
+    expect(responseBody).to.have.keys('errors', 'resource', 'status');
+    expect(responseBody.errors[0]).to.have.keys('id', 'message');
+
+    expect(responseBody.resource).to.be.a('string');
+    expect(responseBody.status).to.be.a('boolean');
+  });
+
+
+  /**
+   * Verify that all rewards are retrieved successfully and have the expected format.
+   * @param {Object} supertest - The supertest object for making HTTP requests.
+   */
+
+  it('should find all the rewords', async function ({ supertest }) {
+
+    const startTime = performance.now();
+
+    const response = await supertest
+      .request(baseUrl)
+      .get('/rewards/getAllRewards')   // https://nashtechglobal.qa.go1percent.com/assets/i18n/en.json
+      .set(headers)
+      .expect('Content-Type', /json/);
+
+    const endTime = performance.now();
+
+    const responseBody = response.body;
+
+    // Assert that the execution time is less than or equal to 3000 milliseconds.
+    expect(endTime - startTime).to.be.at.most(3000); // Adjust the expected range as needed
+
+    for (let indexOfResponse = 0; indexOfResponse < responseBody.length; indexOfResponse++) {
+      const object = responseBody[indexOfResponse];
+
+      // Assert keys
+      expect(object).to.have.all.keys(
+        'id', 'imageFile', 'name', 'pointsNeededToRedeem', 'description', 'rewardType', 'quantity', 'expiryDate', 'active'
+      );
+
+      // Assert data types
+      expect(object.id).to.be.a('number');
+      expect(object.imageFile).to.be.a('string');
+      expect(object.name).to.be.a('string');
+      expect(object.pointsNeededToRedeem).to.be.a('number');
+      expect(object.description).to.be.a('string');
+      expect(object.rewardType).to.be.a('string');
+      expect(object.quantity).to.be.a('number');
+      expect(object.expiryDate).to.be.a('string');
+      expect(object.active).to.be.a('boolean');
+
+
+        // Assert specific values within the response body.
+        expect(object.description).equal('For testing purpose.')
+        expect(object.rewardType).equal('studio');
+        expect(object.quantity).equal(100);
+    }
+
+
+  });
+
+  /**
+   * Verify the points in the points table for a specific user.
+   * @param {Object} supertest - The supertest object for making HTTP requests.
+   */
+
+  it('verify the points in points table', async function ({ supertest }) {
+
+    const startTime = performance.now();
+
+    const response = await supertest
+      .request(baseUrl)
+      .get('/reputation/666')
+      .set(headers)
+      .expect('Content-Type', /json/); // Expect the specific content type
+
+    const endTime = performance.now();
+
+    const responseBody = response.body;
+
+    // Assert that the execution time is less than or equal to 3000 milliseconds.
+    expect(endTime - startTime).to.be.at.most(3000);
+
+    // Assert that the response body contains specific keys.
+    expect(responseBody).to.have.all.keys(
+      'allTimeRank',
+      'knolderEmail',
+      'knolderName',
+      'monthlyRank',
+      'rewardPoints',
+      'score',
+      'scoreBreakDown',
+      'studioId',
+      'studioName')
+
+
+    // Assert specific values within the response body.
+    expect(responseBody.knolderName).to.equal('Test Employee');
+    expect(responseBody.knolderEmail).to.equal('testemployee@nashtechglobal.com');
+    expect(responseBody.monthlyRank).to.equal(1);
+    expect(responseBody.studioName).to.equal("Frontend Competency");
   });
 
 });
