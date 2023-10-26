1d05b3a5e46454c3b860d9455982c7fece7e7f5e
deepansh257 <deepansh.gupta@nashtechglobal.com>
2023-10-26 14:26:35 +0530

    Resolve conflict

--- go1percent/globals.js
+++ go1percent/globals.js
@@ -3,6 +3,11 @@ function startTime(){
     return new Date().getTime()
 }
 
+
+function startTime(){
+    return new Date().getTime()
+}
+
 module.exports = {
 
     before: function(done) {
@@ -13,9 +18,7 @@ module.exports = {
     after: function(done) {
         chromedriver.stop();
         done();
-    },
-
-  
+    },      
     // Upcoming Page
     queryNasher: {
         'pageNumber': '1',
@@ -80,7 +83,9 @@ module.exports = {
     url : 'https://backend-radar.qa.go1percent.com/',
 
     baseurl:'https://backend.qa.go1percent.com',
-
+    base_url : "https://knolx-backend.qa.go1percent.com",
+    source : "https://nashtechglobal.qa.go1percent.com",
+	
     admin: {
         headers: {
         'Authorization': '',
