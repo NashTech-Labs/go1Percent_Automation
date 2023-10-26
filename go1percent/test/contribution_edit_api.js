05c9eba66152a243ac959422d1893fa1286d65ce
jasleenwahi <jasleen.wahi@knoldus.com>
2023-10-26 08:44:51 +0530

    added a reusable function to assert response time, reading token from the terminal

--- go1percent/test/contribution_edit_api.js
+++ go1percent/test/contribution_edit_api.js
@@ -1,20 +1,34 @@
-const headers = require('../globals')
+const url = require('../globals')
+const accessToken = process.argv.indexOf('--token'); //npx nightwatch test/Add_Config.js --env api_testing --token youracesstoken
 describe('api testing', function () {
 
-    //This test sends a GET request to retrieve details of a specific contribution using the contribution ID.
+    const token = process.argv[accessToken + 1];
+    
+    const header ={
+      'Source': 'https://nashtechglobal.qa.go1percent.com',
+      'Authorization': 'Bearer ' + token
+    }
+    // Asserts the response time in a Nightwatch program.
+    const assertResponseTime = (startTime) => {
+      const endTime = Date.now();
+      const responseTime = endTime - startTime;
+      expect(responseTime).to.be.below(10000);
+    }
 
+
+    // This test sends a GET request to retrieve details of a specific contribution using the contribution ID.
     it('click on a certain contribution and see the details', async function({supertest}) {
       const startTime = new Date().getTime();
       await supertest
-        .request(headers.baseurl)
+        .request(url.baseurl)
         .get("/contribution?contributionId=2729")
-        .set(headers.headers)
+        .set(header)
         
         .expect(200)
         .expect('Content-Type', /json/)
         .then(function(response){
 
-          //The test then checks the response body for the presence of specific properties such as "contributionId," "knolderName," "title," and "status."
+          // The test then checks the response body for the presence of specific properties such as "contributionId," "knolderName," "title," and "status."
             expect(Object.keys(response.body).length).to.be.greaterThan(0);
             expect(response.body.data).to.have.property('contributionId');
             expect(response.body.data).to.have.property('knolderName');
@@ -23,21 +37,17 @@ describe('api testing', function () {
 
         });
 
-      const endTime = new Date().getTime();
-      const responseTime = endTime - startTime;
-      //asserting response time.
-      expect(responseTime).to.be.lessThan(2000);
-
+        assertResponseTime(startTime);
     });
 
-    //This test sends a GET request to retrieve knolder contributions with pagination and a limit.
+    // This test sends a GET request to retrieve knolder contributions with pagination and a limit.
     it('on saving changes for a contribution', async function({supertest}) {
         const startTime = new Date().getTime();
         await supertest
 
-          .request(headers.baseurl)
+          .request(url.baseurl)
           .get("/contribution/getKnolderContribution?pageNumber=1&limit=10000")
-          .set(headers.headers)
+          .set(header)
           
           .expect(200)
           .expect('Content-Type', /json/)
@@ -54,10 +64,7 @@ describe('api testing', function () {
 
         });
 
-        const endTime = new Date().getTime();
-        const responseTime = endTime - startTime;
-        //asserting response time.
-        expect(responseTime).to.be.lessThan(2000);
+        assertResponseTime(startTime);
           
       });
 
@@ -66,9 +73,9 @@ describe('api testing', function () {
         const startTime = new Date().getTime();
 
         await supertest
-          .request("https://backend.qa.go1percent.com")
+          .request(url.baseurl)
           .put('/contribution/editContribution')
-          .set(headers.headers)
+          .set(header)
           .send({"title":"Test employee contribution","contributionType":"Research paper","contributionDate":"2023-10-19 00:00:00","urlDetails":"www.courser.org","technologyDetails":"ddcscdcmjjicjdjc hdfiwic jocdjoicwd fcdiocd chojvwofeefhevdvhvhnvjvjdwehuihefkjcnjkvnwbrihfuvbhjbdjfre","contributionId":2727})
           
           .expect(200)
@@ -84,10 +91,7 @@ describe('api testing', function () {
             
         });
 
-        const endTime = new Date().getTime();
-        const responseTime = endTime - startTime;
-        //asserting response time.
-        expect(responseTime).to.be.lessThan(2000);
+        assertResponseTime(startTime);
           
       });
 
