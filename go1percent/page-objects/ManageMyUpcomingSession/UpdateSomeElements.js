02175668071bdca8dc29a4de8264433d77e6c4b0
DivyanshuDilip <divyanshu.jain@nashtechglobal.com>
2023-10-28 01:18:40 +0530

    Updated Codes

--- go1percent/page-objects/ManageMyUpcomingSession/UpdateSomeElements.js
+++ go1percent/page-objects/ManageMyUpcomingSession/UpdateSomeElements.js
@@ -2,70 +2,182 @@ const My_Upcoming_Session = require('./My_Upcoming_Session')
 
 require('./My_Upcoming_Session')
 
-const Startingbox=My_Upcoming_Session.elements.StartingBox
+const Startingbox = My_Upcoming_Session.elements.ListPresent
+//var newText = existingText + " Your new information here"
 
-module.exports={
+module.exports = {
 
-elements:{
+    elements: {
 
-     EditButton:{
-  
-        locateStrategy: 'xpath',
-        selector:'(//button[text()=" Edit "])[1]',
+        EditButton: {
 
-     },
+            // locateStrategy: 'Xpath',
+            selector: '.btn.btn-primary.title',
 
-TextBox:{
+        },
 
-//locateStrategy:'Xpath',
-selector:'textarea[type="text"]',
+        TextBox: {
 
-
-},
-
-SaveButton:{
-
-locateStrategy:'Xpath',
-selector:'(//button[text()=" Save "])[1]'
-
-},
-
-ToastMessage:{
-
-    locateStrategy:'Xpath',
-    selector:'//div[@aria-label="Successfully Updated"]'
+            //locateStrategy:'Xpath',
+            selector: 'textarea[type="text"]',
 
 
-}
+        },
 
-},
+        SaveButton: {
 
-commands:[{
+            //locateStrategy:'Xpath',
+            selector: '.btn.btn-primary.saveBtn.mt-3',
 
-ClickOnAnySession(){
+        },
 
-return this
-.waitForElementVisible(Startingbox,3000)
-.click(Startingbox)
-.waitForElementVisible('@EditButton',3000)
-.click('@EditButton')
-.waitForElementPresent('@TextBox',5000)
-.click('@TextBox')
-.pause(2000)
-.setValue('@TextBox','New Session')
-.waitForElementPresent('@SaveButton',3000)
-.click('@SaveButton')
-.waitForElementVisible('@ToastMessage',3000)
+        ToastMessage: {
 
-}
+            //locateStrategy:'Xpath',
+            selector: 'div[aria-label="Successfully Updated"]'
+        },
+
+        Tags: {
+
+            selector: 'a[role="button"]'
+        },
+
+        AddTagTextBox: {
+
+            selector: 'input[type="text"]',
+
+        },
+
+        SaveNewTag: {
+
+            selector: 'button[type="submit"]',
+        },
+
+        RemoveTag: {
+
+            selector: '.fa.fa-times',
+
+        },
+
+        DescriptionEditButton: {
+
+            selector: '.btn.btn-primary.edit',
+
+        },
+
+        DescriptionTextBox: {
+
+            selector: 'textarea[type="text"]',
+
+        },
+
+        DescriptionSaveButton: {
+
+            selector: '.btn.btn-primary.saveBtn.m-2',
+
+        },
+
+
+
+
+    },
+
+
+    commands: [{
+
+        ClickOnAnySession() {
+
+            return this
+                .pause(5000)
+                .waitForElementVisible(Startingbox, 5000)
+                .click(Startingbox)
+                .pause(5000)
+                //.waitForElementVisible('@EditButton',5000)
+                .click('@EditButton')
+                .waitForElementPresent('@TextBox', 5000)
+                .click('@TextBox')
+                .pause(2000)
+                .setValue('@TextBox', 'New Session')
+                .waitForElementPresent('@SaveButton', 5000)
+                .click('@SaveButton')
+                .waitForElementVisible('@ToastMessage', 5000)
+
+        },
+
+        WithoutTitle() {
+
+            return this
+                .pause(4000)
+                .waitForElementVisible(Startingbox, 3000)
+                .click(Startingbox)
+                .pause(4000)
+                .waitForElementVisible('@EditButton', 3000)
+                .click('@EditButton')
+                .waitForElementPresent('@TextBox', 5000)
+                .click('@TextBox')
+                .clear()
+
+        },
+
+        AddNewTag() {
+
+            return this
+
+                .pause(4000)
+                .waitForElementVisible(Startingbox, 3000)
+                .click(Startingbox)
+                .waitForElementPresent('@Tags', 4000)
+                .click('@Tags')
+                .waitForElementVisible('@AddTagTextBox', 4000)
+                .setValue('@AddTagTextBox', 'Automation')
+                .click('@SaveNewTag')
+
+        },
+
+        RemoveNewTag() {
+
+            return this
+
+                .pause(4000)
+                .waitForElementVisible(Startingbox, 3000)
+                .click(Startingbox)
+                .waitForElementVisible('@RemoveTag', 3000)
+                .click('@RemoveTag')
+
+        },
+
+        UpdateDescriptionBox() {
+            var existingText
+            var newText
+            return this
+                .pause(4000)
+                .waitForElementVisible(Startingbox, 3000)
+                .click(Startingbox)
+                .waitForElementPresent('@DescriptionEditButton', 3000)
+                .pause(5000)
+                .click('@DescriptionEditButton')
+                .pause(5000)
+                .waitForElementVisible('@DescriptionTextBox', 3000)
+                .click('@DescriptionTextBox')
+                .pause(10000)
+                .getText('@DescriptionTextBox', function(result) {
+                    existingText = result.value; 
+                     newText = existingText + " Your new information here";
+                  })
+                  .setValue('@DescriptionTextBox', newText)
+                  
+                //  .setValue('@DescriptionTextBox', newText)
+                 .pause(10000)
+
+                // .waitForElementPresent('@DescriptionSaveButton', 3000)
+                // .click('@DescriptionSaveButton')
+        }
 
 
 
 
 
-
-
-}]
+    }]
 
 
 
--- go1percent/test/Manage_My_Upcoming_Session_API_And_FE/Upcoming-Session-FrontEnd.js
+++ go1percent/test/Manage_My_Upcoming_Session_API_And_FE/Upcoming-Session-FrontEnd.js
@@ -5,7 +5,7 @@ const Upcoming = browser.page.My_Upcoming_Session()
 const Update=browser.page.UpdateSomeElements()
 
 describe("Manage My Upcoming Session", () => {
-    before((function (browser) {
+    beforeEach((function (browser) {
         browser
             .maximizeWindow()
             .page.login()
@@ -21,28 +21,70 @@ describe("Manage My Upcoming Session", () => {
 
     })),
 
-        it('verify that booked session should be visible in the my upcoming session (TC-177)', function () {
+        // it('verify that booked session should be visible in the my upcoming session (TC-177)', function () {
 
-            Upcoming
-                .ClickOnUpcomingSession()
-                .assert.containsText('@UpcomingSession', 'My Upcoming Session')
-                .assert.containsText('@StartingBox', 'testing')
-                .assert.containsText('@MiddleBox', 'Test Title 4892')
-                .assert.containsText('@LastBox', 'TestAutomationTitleNashtech')
+        //     Upcoming
+        //         .ClickOnUpcomingSession()
+        //         // .assert.containsText('@UpcomingSession', 'My Upcoming Session')
+        //         // .assert.containsText('@StartingBox', 'testing')
+        //         // .assert.containsText('@MiddleBox', 'Test Title 4892')
+        //         // .assert.containsText('@LastBox', 'TestAutomationTitleNashtech')
+        //         .assert.elementPresent('@ListPresent')
 
 
-        }),
+        // }),
 
-        it('Verify that user should able to update the title (TC-178) ',function(){
+        // it('Verify that user should able to update the title (TC-178) ',function(){
+
+        //     Update
+        //     .ClickOnAnySession()
+        //     .assert.containsText('@ToastMessage','Successfully Updated')
+        
+
+        // }),
+
+        // it(' Verify that user should not able to save the session without title (TC-179)',function(){
+
+        //       Update
+        //       .WithoutTitle()
+        //       .assert.attributeEquals('@SaveButton', 'disabled', 'disabled')
+
+
+
+
+        // }),
+
+        // it('verify that user should able to add tags (TC-180)',function(){
+
+        //      Update
+        //      .AddNewTag()
+        //      .assert.containsText('@ToastMessage','Successfully Updated')
+
+        // }),
+         
+        // it(' verify that user should able to remove the added tags (TC-181)',function(){
+
+        //       Update
+        //       .RemoveNewTag()
+        //       .assert.containsText('@ToastMessage','Successfully Updated')
+
+
+        // }),
+
+        it('verify that user should able to update the description (TC-182)',function(){
 
             Update
-            .ClickOnAnySession()
-            .assert.containsText('@ToastMessage','Successfully Updated')
+            .UpdateDescriptionBox()
+           // .assert.containsText('@ToastMessage','Successfully Updated')
         
+            
 
         })
 
-        after(function (browser) {
+
+
+
+        afterEach(function (browser) {
             browser.end();
         })
 
