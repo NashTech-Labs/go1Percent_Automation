71d78183ed4fd0beca1608e3819b47de9fe4e880
Manas-NTech <manas.satapathy@nashtechglobal.com>
2023-10-31 19:44:40 +0530

    FE changes

---
+++ go1percent/test/FE/demo.js
@@ -0,0 +1,20 @@
+//it('My Profile Page load', function(done){
+        //    this.timeout(25000);
+        //    setTimeout(done, 25000);
+        //
+   //
+        //browser
+        //    .page.myProfilePage()
+        //    //.ClickOnSetting()
+        //    .ClickOnProfilePic()
+        //    //.waitForPageLoad()
+        //
+        //    .pause(3000)
+        //    .assert.urlContains("My-Profile",'URL contains my-profile');
+        //    browser
+        //    .pause(5000)
+        //    .saveScreenshot('tests_output/My_Profile.png');
+        //    callback();
+        //});
+        //assert.textContains('@var','text');
+        //browser.ensure.elementIsVisible('@var')
