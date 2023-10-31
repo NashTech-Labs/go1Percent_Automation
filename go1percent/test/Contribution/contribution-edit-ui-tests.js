ffad3d7bbf6771ba1c112cb1e35aa25b4fce5ee3
jasleenwahi <jasleen.wahi@knoldus.com>
2023-10-31 18:23:23 +0530

    added comments

--- go1percent/test/Contribution/contribution-edit-ui-tests.js
+++ go1percent/test/Contribution/contribution-edit-ui-tests.js
@@ -3,6 +3,7 @@ const { assert } = require("chai");
 module.exports = {
 
     before: (client) => {
+         // Actions to be performed before running the test cases, such as maximizing the window and logging in.
         client
             .maximizeWindow()
             .page.login()
@@ -14,6 +15,7 @@ module.exports = {
             .click('@profile')
      },
     
+    // This test case verifies that save button is disabled if a title less than 15 words is entered. 
     "Verify that the save button should be disable if user add a title less than 15 words" : (browser) => {
         const editContribution = browser.page.Contribution.contribution_page_object();
         editContribution
@@ -29,6 +31,7 @@ module.exports = {
 
     },
 
+    // This test case verifies that save button is disabled if a description less than 100 words is entered.
     "Verify that the save button should be disable if user add a description less than 100 words" : (browser) => {
         const editContribution = browser.page.Contribution.contribution_page_object();
         editContribution
@@ -44,6 +47,7 @@ module.exports = {
 
     },
 
+    // This test case verifies that save button is disabled if invalid url is entered.
     "Verify that the save button should not be enable if user add a invalid URL" : (browser) => {
         const editContribution = browser.page.Contribution.contribution_page_object();
         editContribution
@@ -58,6 +62,7 @@ module.exports = {
             .expect.element('@saveButton').to.have.attribute('disabled');
     },
 
+    // This test case verifies that contribution type cannot be edited.
     "Verify that user should not be able to edit the contribution type of the added contribution" : (browser) => {
         const editContribution = browser.page.Contribution.contribution_page_object();
         editContribution
@@ -71,6 +76,7 @@ module.exports = {
 
     },
 
+    // This test case verifies that title can be edited.
     "Verify that user should be able to edit the title of the added contribution" : (browser) => {
         const editContribution = browser.page.Contribution.contribution_page_object();
         editContribution
@@ -84,6 +90,7 @@ module.exports = {
 
     },
     
+    // This test case verifies that description can be edited.
     "Verify that user should be able to edit the details of pending contribution" : (browser) => {
         const editContribution = browser.page.Contribution.contribution_page_object();
         editContribution
@@ -97,6 +104,7 @@ module.exports = {
 
     },
 
+    // This test case verifies that contribution date can be edited.
     "Verify that user should be able to edit the contribution date of the added contribution except online course" : (browser) => {
         const editContribution = browser.page.Contribution.contribution_page_object();
         editContribution
@@ -110,6 +118,7 @@ module.exports = {
 
     },
 
+    // This test case verifies that save button is disabled if user leaves any field blank.
     "Verify that save button should be disabled if user leaves any field blank" : (browser) => {
         const editContribution = browser.page.Contribution.contribution_page_object();
         editContribution
@@ -124,6 +133,7 @@ module.exports = {
 
     },
 
+    // This test case verifies that contribution date of online course type contribution cannot be edited.
     "Verify that date of the online course type contribution cannot be modified": (browser) => {
         const editContribution = browser.page.Contribution.contribution_page_object();
         editContribution
@@ -138,6 +148,7 @@ module.exports = {
 
     },
     
+    // This test case verifies that title/description/url of online course type contribution cannot be edited.
     "Verify that the title/description/url of the online course contribution cannot be modified": (browser) => {
         const editContribution = browser.page.Contribution.contribution_page_object();
         editContribution
