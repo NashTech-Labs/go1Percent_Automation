5138b6fbba2573ba62df494023122f5932e9c69c
tarunrawatknoldus <tarun.rawat@knoldus.com>
2023-10-31 13:28:45 +0530

    API Updated

--- go1percent/globals.js
+++ go1percent/globals.js
@@ -169,6 +169,17 @@ module.exports = {
 
         },
     },
+
+    dataToUpdate: {
+        sessionDescription: "The error message indicates that the property 'topic' is not present in the response body, which is why the assertion is failing. This could be due to the structure of the response body or the way the API is handling the request. Please ensure that the API response structure matches the expected response format.",
+        feedbackFormName: "sdv",
+        sessionId: "653105778555d37c0a4f8d96",
+        remarks: "have not updated the sessions",
+        slideURL: "www.google.com",
+        sessionTag: ["Python"],
+        topic: "Updated Title",
+        saveOption: true
+    },
          
     urls: {
             token: "https://auth.go1percent.com/auth/realms/nashtech/protocol/openid-connect",
