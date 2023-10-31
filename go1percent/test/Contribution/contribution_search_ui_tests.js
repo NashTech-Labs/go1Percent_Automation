e9b8f7dd2125d76983e918d8c441079cf78f8026
jasleenwahi <jasleen.wahi@knoldus.com>
2023-10-31 18:23:23 +0530

    added comments

--- go1percent/test/Contribution/contribution_search_ui_tests.js
+++ go1percent/test/Contribution/contribution_search_ui_tests.js
@@ -3,6 +3,7 @@ const { assert } = require("chai");
 module.exports = {
 
     before: (browser) => {
+        // Perform actions before running test cases, such as maximizing the browser window and logging in.
         browser
             .maximizeWindow()
             .page.login()
@@ -12,7 +13,8 @@ module.exports = {
             .signIn()
             
      },
-
+     
+     // This test case verifies that search bar is clickable.
     "Verify that user is able to click on the search bar": (browser) => {
         const searchContribution = browser.page.Contribution.contribution_page_object();
         searchContribution
@@ -23,6 +25,7 @@ module.exports = {
             .clickSearchBar()
     },
 
+    // This test case verifies that we are able to filter out contributions based on a keyword entered.
     "Verify that user should be able to filter any certain contribution from the list": (browser) => {
         const searchContribution = browser.page.Contribution.contribution_page_object();
         searchContribution
@@ -33,11 +36,13 @@ module.exports = {
             .clickSearchBar()
             .searchContribution('test contribution')
 
+            // Use browser.elements to find elements and check if there are more than 0 of them.
             browser.elements('css selector', 'div.studio-member div > div > div.col-xxl-3.col-lg-3.col-md-3.col-sm-6.d-flex.flex-column.text-center', function(result) {
                 expect(result.value.length).to.be.greaterThan(0);
             })
     },
 
+    // This test case verifies that if a wrong keyword is entered it gives no results.
     "Verify that user should see a blank screen when they enter invalid character that is not present": (browser) => {
         const searchContribution = browser.page.Contribution.contribution_page_object();
         searchContribution
@@ -48,6 +53,7 @@ module.exports = {
             .clickSearchBar()
             .searchContribution('app')  
 
+            // Use browser.elements to find elements and check if there are no elements.
             browser.elements('css selector', 'div.studio-member div > div > div.col-xxl-3.col-lg-3.col-md-3.col-sm-6.d-flex.flex-column.text-center', function(result) {
                 expect(result.value.length).to.be.equal(0);
             })  
