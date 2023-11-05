fc913eaaf57b763029ca919f047d75eadb9f75ea
Manas-NTech <manas.satapathy@nashtechglobal.com>
2023-11-06 02:14:43 +0530

    modified

--- go1percent/test/myProfile_API/MyProfile_API.js
+++ go1percent/test/myProfile_API/MyProfile_API.js
@@ -1,14 +1,15 @@
 const globals = require('../../globals');
 const headers = globals.admin.headers;
 const token_body = globals.admin.tokenBody;
-const urls = globals.urls;
+const token_urls = globals.urls;
+const urls = globals.Add_Configuration.BaseUrl;
 const token_headers = globals.admin.tokenHeaders;
 
 describe('MyProfile API', function () {
     it('Configuring Bearer Token', async function ({ supertest }) {
         // const startTime = performance.now();
         await supertest
-          .request(urls.token)
+          .request(token_urls.token)
           .post("/token")
           .send(token_body)
           .set(token_headers)
@@ -22,14 +23,15 @@ describe('MyProfile API', function () {
     it('Check All Rewards Data', async function ({ supertest }) {
         const startTime = performance.now();
         const response = await supertest
-            .request(headers.base_url)
-            .get('rewards/getAllRewards')
+            .request(urls)
+            .get('/rewards/getAllRewards')
             .set( headers)
-            .expect(200);
+            .expect(200)
+            .expect('Content-Type', /json/);
 
         const endTime = performance.now();
         const responseTime = endTime - startTime;
-        expect(responseTime).to.be.lessThan(2000);
+        expect(responseTime).to.be.lessThan(8000);
 
         const rewards = response.body.rewards;
         expect(rewards).to.satisfy((rewards) => {
@@ -40,57 +42,43 @@ describe('MyProfile API', function () {
         }, 'All rewards should have "name" and "description" properties that are strings');
     });
 
-    // it('Check all Knolder ID', async function ({ supertest }) {
-    //     const startTime = performance.now();
-    //     const response = await supertest
-    //         .request(headers.url)
-    //         .get('/get/knolderId?knolderEmail=testadmin@nashtechglobal.com')
-    //         .expect(200);
-    //         const expectedStudioIds = [
-    //             'ALL_STUDIO',
-    //             'AGILE_COMPETENCY',
-    //             'AI_ML_COMPETENCY',
-    //             'DEVOPS_COMPETENCY',
-    //             'FRONTEND_COMPETENCY',
-    //             'JAVA_COMPETENCY',
-    //             'SCALA_COMPETENCY',
-    //             'TEST_AUTOMATION_COMPETENCY'
-    //         ];
-    //     const endTime = performance.now();
-    //     const responseTime = endTime - startTime;
-    //     expect(responseTime).to.be.lessThan(2000, 'Expected response time to be less than 2000ms');
+    it('Check all Knolder ID', async function ({ supertest }) {
+        const startTime = performance.now();
+        const response = await supertest
+            .request(urls)
+            .get('/get/knolderId?knolderEmail=testadmin@nashtechglobal.com')
+            .set( headers)
+            .expect(200)
+            .expect('Content-Type', /json/);
+            
+        const endTime = performance.now();
+        const responseTime = endTime - startTime;
+        expect(responseTime).to.be.lessThan(2000, 'Expected response time to be less than 2000ms');
 
-    //     const studios = response.body.data;
+        const resources = response.body.data;
+        expect(resources).to.satisfy((resource) => {
+            return resources.every(reward => {
+                return resources.hasOwnProperty('resource') && typeof resources.name === 'string';
+            });
+        }, 'All knolder ID should have resource property that is string');
+    });
 
-    //     const missingCompetencies = studios.filter(studio => !studio.studioId || !expectedStudioIds.includes(studio.studioId));
-    //     expect(missingCompetencies.length, 'All competencies should match the expected values').to.equal(0);
-    // });
+    it('Get Profile Pic', async function ({ supertest }) {
+        const startTime = performance.now();
+        const response = await supertest
+            .request(urls)
+            .get('/profile/getProfilePic?email=testadmin@nashtechglobal.com')
+            .set(headers)
+            .expect(200);
+        const endTime = performance.now();
+        const responseTime = endTime - startTime;
+        expect(responseTime).to.be.lessThan(2000);
 
-    // it('Get Profile Pic', async function ({ supertest }) {
-    //     const startTime = performance.now();
-    //     const response = await supertest
-    //         .request(headers.base_url)
-    //         .get('/profile/getProfilePic?email=testadmin@nashtechglobal.com')
-    //         .set('source', headers.source)
-    //         .set('authorization', headers.access_token)
-    //         .expect(200);
-    //     const endTime = performance.now();
-    //     const responseTime = endTime - startTime;
-    //     expect(responseTime).to.be.lessThan(2000);
-
-    //     const slots = response.body.slots;
-
-    //     const expectedDataTypes = {
-    //         id: 'string',
-    //         message: 'string'
-    //     };
-
-    //     const dataTypesMatch = slots.every(slot => {
-    //         return Object.keys(expectedDataTypes).every(field => {
-    //             return typeof slot[field] === expectedDataTypes[field];
-    //         });
-    //     });
-
-    //     expect(dataTypesMatch, 'All fields have correct data types').to.be.true;
-    // });
+        const resources = response.body.data;
+        expect(resources).to.satisfy((resource) => {
+            return resources.every(reward => {
+                return resources.hasOwnProperty('resource') && typeof resources.name === 'string';
+            });
+        }, 'All Profile Picture should have rresource property that is string');
+    });
 });
