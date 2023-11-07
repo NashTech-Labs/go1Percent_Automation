76b28165295c9b3f2ef77d85d2d52a42c5a61545
Anshika1603 <anshika.srivastava@knoldus.com>
2023-11-07 12:49:35 +0530

    Modified page object

--- go1percent/nightwatch.conf.js
+++ go1percent/nightwatch.conf.js
@@ -27,7 +27,7 @@ module.exports = {
 
 
   // See https://nightwatchjs.org/guide/concepts/page-object-model.html
-  page_objects_path: ['page-objects', 'page-objects/Techhub_AdminUser_FE', 'page-objects/Techhub_EmployeeUser_FE'],
+  page_objects_path: ['page-objects', 'page-objects/Techhub_AdminUser_FE'],
 
   // See https://nightwatchjs.org/guide/extending-nightwatch/adding-custom-commands.html
 
