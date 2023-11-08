d69bd3e49d17e1b4f414412b07ac332a6ca166ca
mayankKhokhar <mayank.khokhar@nashtechglobal.com>
2023-11-08 15:54:41 +0530

    Updated a XPath for faster execution

--- go1percent/page-objects/bookASessionPage.js
+++ go1percent/page-objects/bookASessionPage.js
@@ -30,7 +30,7 @@ module.exports = {
             locateStrategy: 'xpath'
         },
         firstAvailableDate: {
-            selector: '(//*[@class="bg-unavailable text-white bg-available ng-star-inserted"])[last()]',
+            selector: '(//span[@class="bg-unavailable text-white bg-available ng-star-inserted"])[last()]',
             locateStrategy: 'xpath'
         },
         testAutomationOption: {
