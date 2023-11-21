6955691d8ee9ead8e9afa652c97fafbb2c656502
vanshika1221 <vanshika.srivastava@knoldus.com>
2023-11-21 17:33:01 +0530

    Added describe and it in test cases

--- go1percentTS/src/test/Go1PercentFEAutomation/LeaderboardRewards/LeaderboardUser.ts
+++ go1percentTS/src/test/Go1PercentFEAutomation/LeaderboardRewards/LeaderboardUser.ts
@@ -1,12 +1,12 @@
-import { NightwatchTests, NightwatchBrowser } from "nightwatch";
+import { NightwatchBrowser } from "nightwatch";
 import{LoginPage} from '../../../page-objects/login';
 import{RewardsPage} from '../../../page-objects/LeaderboardRewards/RewardSectionPage';
 import globalsData from '../../../../globals';
 
 let sessionsPage: RewardsPage;
-const RewardsPageUIAutomationUser: NightwatchTests = {
+describe("Rewards Page Frontend Automation with Typescript for User", () => {
     
-    before: (browser: NightwatchBrowser) => {
+    before((browser: NightwatchBrowser) => {
         const page = browser.page.login()as LoginPage;
         page
             .maximizeWindow()
@@ -17,27 +17,27 @@ const RewardsPageUIAutomationUser: NightwatchTests = {
 
             sessionsPage = browser.page.LeaderboardRewards.RewardSectionPage() as RewardsPage;
             
-    },
+    }),
 
     // Verify that user should be able to click reward button and able to see reward page (TC-273) 
-    "Should be able to see rewards page on expanding leaderboard and clicking rewards button": () => {
+    it("Should be able to see rewards page on expanding leaderboard and clicking rewards button", () => {
         sessionsPage
         .clickOnLeaderboardButton()
         .clickOnRewardsButton()
         .assert.urlContains("/rewards/list");
-    }, 
+    }), 
 
     // Verify user should able to see and switch between Two buttons (TC-274) 
-    "Should be able to switch between forCompetency and Individual Buttons": () => {
+    it("Should be able to switch between forCompetency and Individual Buttons", () => {
         sessionsPage
            .clickOnForCompetencyButton()
            .assert.cssProperty('@currentButton','background-color',globalsData.rewardSectionMessages.colorCode)
            .clickOnIndividualButton()
            .assert.cssProperty('@currentButton','background-color',globalsData.rewardSectionMessages.colorCode)   
-    },
+    }),
 
     // Verify that user should be able to see all the Rewards present in Rewards page (TC-275)
-    "Should be able to see reward points on loading all rewards": () => {
+    it("Should be able to see reward points on loading all rewards", () => {
         const resultMessage = "pts"
         sessionsPage
             .assert.textContains('@points',resultMessage)   
@@ -46,36 +46,34 @@ const RewardsPageUIAutomationUser: NightwatchTests = {
                 this.assert.ok(value > 0, 'Value is greater than 0'); // Assert that the value is greater than 0
               });
           
-    },
+    }),
 
     // Verify that user should able to click on Redeem button (TC-276)
-    "Should be able to click on redeem button": () => {
+    it("Should be able to click on redeem button", () => {
         sessionsPage
            .clickOnRedeemButton()
            .assert.textContains('@popup',globalsData.rewardSectionMessages.popupMessage)
-    },
+    }),
 
      // Verify that user should able to click on NO button (TC-278) 
-     "Should be able to close popup message box when no button is clicked": (browser: NightwatchBrowser) => {
+     it("Should be able to close popup message box when no button is clicked", (browser: NightwatchBrowser) => {
         sessionsPage
             .clickOnNoButton()
             .assert.not.elementPresent('@popupBox')
                 
-    },
+    }),
 
     // Verify that user should able to click on yes button (TC-277) 
-    "should display message when yes button is clicked": () => {
+    it("Should display message when yes button is clicked", () => {
         sessionsPage
             .waitForElementPresent('@redeemButton')
             .clickOnRedeemButton()
             .clickOnYesButton()
             .assert.textContains('@message',globalsData.rewardSectionMessages.redeemMessage)
                 
-    },
+    }),
     
-    after: (browser: NightwatchBrowser) => {
+    after((browser: NightwatchBrowser) => {
         browser.end();
-    },
-};
-
-export default RewardsPageUIAutomationUser;
+    })
+})
