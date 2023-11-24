bc44b24f6977ebba096bec37129b0ff2c1a7a3cd
juliKumari <juli.kumari@nashtechglobal.com>
2023-11-24 11:13:19 +0530

    add base qa url in the globals

--- go1percentTS/src/globals.ts
+++ go1percentTS/src/globals.ts
@@ -1,34 +1,39 @@
 // globals.ts
 
 const techhubUrls = {
-    token: "https://auth.go1percent.com/auth/realms/nashtech/protocol/openid-connect",
-    go1percentBase: "https://techhub-backend.qa.go1percent.com",
-  };
-  
-  const headers = {
-    'Authorization': '',
-    'Source': 'https://nashtechglobal.qa.go1percent.com'
-  };
-  
-  const tokenHeaders = {
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'source': 'https://nashtechglobal.qa.go1percent.com'
-  };
-  
-  const tokenBody = {
-    client_id: 'leaderboard-ui',
-    client_secret: '8090ed15-4cd1-483c-9fee-2a8b35941852',
-    username: 'testadmin',
-    password: 'testadmin',
-    grant_type: 'password',
-  };
-  
-  export const admin = {
-    techhubUrls,
-    headers,
-    tokenHeaders,
-    tokenBody,
-  };
-  
-  // ... (other exports)
-  
+  token: "https://auth.go1percent.com/auth/realms/nashtech/protocol/openid-connect",
+  go1percentBase: "https://techhub-backend.qa.go1percent.com",
+};
+
+const headers = {
+  'Authorization': '',
+  'Source': 'https://nashtechglobal.qa.go1percent.com'
+};
+
+const tokenHeaders = {
+  'Content-Type': 'application/x-www-form-urlencoded',
+  'source': 'https://nashtechglobal.qa.go1percent.com'
+};
+
+const tokenBody = {
+  client_id: 'leaderboard-ui',
+  client_secret: '8090ed15-4cd1-483c-9fee-2a8b35941852',
+  username: 'testadmin',
+  password: 'testadmin',
+  grant_type: 'password',
+};
+// Backend QA Url
+const base_url ="https://knolx-backend.qa.go1percent.com/"
+
+export const admin = {
+  techhubUrls,
+  headers,
+  tokenHeaders,
+  tokenBody,
+};
+
+export const baseURL ={
+  base_url
+}
+
+// ... (other exports)
