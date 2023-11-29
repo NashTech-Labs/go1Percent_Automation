29879491af7844c4f17d8d599dcff8b91e62e0fa
souravmishraa <sourav.mishra@nashtechglobal.com>
2023-11-29 09:59:11 +0530

    newcodeadded

---
+++ go1percent/page-objects/settingPage/settingPage.js
@@ -0,0 +1,385 @@
+
+module.exports = {
+    url: "",
+    elements: {
+        profileSetting:"ul[class='navbar-nav justify-content-end'] app-dropdown-wrapper div[class='position-relative'] div[class='pt-2'] li[class='icon nav-item dropdown px-2 d-flex align-items-center justify-content-center'] a[class='nav-link text-body p-0 text-white'] div i[class='material-icons user-icon']",
+        clickonprofile:".d-flex.py-1.mt-1",
+        clickonset:".tab-icon[src='../../../assets/Icons/Settings.svg']",
+        seeallfield:".card.tab-card.py-4.px-5",
+        editclickname:"div[class='left-div-button-div mt-1'] button[class='edit-detail-button']",
+        fullName:"div.col-lg-8 app-my-settings div.float-container div:nth-child(1) div:nth-child(2) input",
+        savebutt:"div[class='left-div-button-div mt-1'] button:nth-child(1)",
+        errormsg:"div:nth-child(1) div.left-div-button-div.mt-1 span",
+        updategithubid:"app-my-settings div:nth-child(5) > button",
+        githubup:".my-4 input",
+        githubsave:"app-my-profile div:nth-child(2) div:nth-child(3) app-my-settings div:nth-child(1) div:nth-child(2) div:nth-child(1) div:nth-child(4) button:nth-child(1)",
+        errormsggit:"div.float-container > div > div.d-flex.flex-column.flex-md-row.my-4 > div:nth-child(1) > div:nth-child(3) > span",
+        gitcancel:"div[class='card-gap'] button:nth-child(2)",
+        wordpresssave:"div:nth-child(3) div.left-div-button-div.mt-2 div button:nth-child(1)",
+        textboxwordpress:"div.col-lg-8 > div:nth-child(3) > app-my-settings div.float-container div div.mt-3.d-flex.flex-column.flex-md-row.justify-content-between > div:nth-child(3) > div:nth-child(2) > input",
+        updatewordpress:"div.col-lg-8 > div:nth-child(3) > app-my-settings div.float-container div div.mt-3.d-flex.flex-column.flex-md-row.justify-content-between > div:nth-child(3) > div.left-div-button-div.mt-2 > button",
+        savedropdown:"div[class='mt-3 d-flex flex-column flex-md-row justify-content-between'] div:nth-child(2) button:nth-child(1)",
+        selectdropdown:"option[value='11']",
+        dropdownloc:"select[placeholder='Your studio ID']",
+        updatecompetency:"div.col-lg-8 > div:nth-child(3) > app-my-settings div.float-container div div.mt-3.d-flex.flex-column.flex-md-row.justify-content-between > div:nth-child(2) > select",
+        changecompetency:"div[class='mt-3 d-flex flex-column flex-md-row justify-content-between'] div:nth-child(2) button:nth-child(1)",
+        changepractice:"app-my-profile:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div:nth-child(3) > app-my-settings:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(3) > button:nth-child(1)",
+        updatetextpractice:"select[placeholder='Your studio ID']",
+        selectpractice:"option[value='Quality Solutions']",
+        savebuttpractice:"app-my-profile:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div:nth-child(3) > app-my-settings:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(4) > button:nth-child(1)",
+        addskill:"app-my-profile > div > div.col-lg-8 > div:nth-child(3) > app-my-settings > div > div.url-div.mt-3.d-flex.mb-4 > div:nth-child(1) > form > div > div > div:nth-child(2) > input",
+        removeskill:"div.col-lg-8 app-my-settings ul li button i",
+        updatelocation:"app-my-profile > div > div.col-lg-8 > div:nth-child(3) > app-my-settings > div > div.float-container > div > div.d-flex.flex-column.flex-md-row.my-4 > div:nth-child(3) > div.left-div-button-div.mt-2 > button",
+        selectdroploc:"select[placeholder='Your location']",
+        locup:"option[value='India']",
+        errorspe:".left-div-button-div.mt-1",
+        errorMsg: '.error-message',
+        saveloc:"app-my-profile:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div:nth-child(3) > app-my-settings:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(3) > div:nth-child(4) > button:nth-child(1)        ",
+    },
+ 
+    commands: [{
+        waitForPageLoad() {
+            return this
+                .waitForElementVisible('body', );
+        },
+ 
+          clickOnElement(element) {
+              return this
+                  .waitForElementVisible(element,10000)
+                  .click(element)
+                  .waitForPageLoad();
+         },
+             clickOn(){
+                return this
+                .clickOnElement("@profileSetting")
+                .waitForElementVisible("@clickonprofile", 3000)  
+                .clickOnElement("@clickonprofile")
+                .waitForElementVisible("@clickonset", 8000)  
+                .clickOnElement("@clickonset")
+                .waitForPageLoad();
+            
+        },
+        verifyAllFieldsVisible() {
+            return this
+              .waitForPageLoad()
+              .verify.visible('@seeallfield')
+              .waitForPageLoad();           
+          },
+        verifyeditname(newFullName) {
+             return this
+                 .clickOnElement('@editclickname')
+                 
+                 .clickOnElement('@fullName')
+                  .setValue('@fullName', newFullName)
+                 
+                  .clickOnElement('@savebutt') 
+                 
+                  .perform((done) => {
+                     this.api.getAlertText((result) => {
+                         const alertText = result.value;
+                         if (alertText === 'Profile updated successfully!') {
+                             console.log('Profile updated successfully!');
+                         } else {
+                             console.log('Profile update failed. Alert text:', alertText);
+                         }
+                         done();
+                     });
+                 })
+                   .waitForPageLoad() 
+                   }, 
+        verifyChangesNot() {
+                    this.clickOnElement('@editclickname');
+                    try {
+                        this.setValue('@fullName', 'Test Employee1');
+                        console.log('Field edited without clicking change button. Test failed!');
+                    } catch (error) {
+                        
+                        console.log('Expected behavior: Field is not editable without clicking change button');
+                    }
+                    return this;
+                }, 
+        verifyAlphabetsNotall() {
+                    return this
+                    .setValue('@fullName', '123') 
+                    .click('@savebutt') 
+                    .waitForElementVisible('@errormsg', 20000)
+                    .assert.containsText('@errormsg', 'Name can only contain alphabets')
+                                     
+                  }, 
+        verifyAllGitandWordpress() {
+        return this
+        .clickOnElement('@updategithubid')
+        .clearValue('@githubup') 
+        .setValue('@githubup', ' ') 
+        .click('@githubsave')
+        .waitForElementVisible('@errormsggit', 5000)
+        .assert.containsText('@errormsggit', 'GitHub Id cannot contain whitespaces'.trim())          
+        },   
+        verifyGit(newGITid) {
+            return this
+                .clickOnElement('@githubup')
+                 .setValue('@githubup', newGITid)
+                
+                 .clickOnElement('@githubsave') 
+                 .perform((done) => {
+                    this.api.getAlertText((result) => {
+                        const alertText = result.value;
+                        if (alertText === 'Profile updated successfully!') {
+                            console.log('Profile updated successfully!');
+                        } else {
+                            console.log('Profile update failed. Alert text:', alertText);
+                        }
+                        done();
+                    });
+                    
+                })
+                .waitForPageLoad() 
+                
+    },
+    verifyGitclone(newGITidclone) {
+        return this
+             .clickOnElement('@updategithubid')
+             .clickOnElement('@githubup')
+             .setValue('@githubup', newGITidclone)
+             .clickOnElement('@githubsave') 
+             .perform((done) => {
+                this.api.getAlertText((result) => {
+                    const alertText = result.value;
+                    if (alertText === 'Cannot update provided github id , as it already belongs to other user') {
+                        console.log('Cannot update provided github id , as it already belongs to other user');
+                    } else {
+                        console.log('Profile update failed. Alert text:', alertText);
+                    }
+                    done();
+                });
+                
+            })
+            .waitForPageLoad() 
+           
+},
+verifywordpress(newwordpress) {
+    return this
+        .clickOnElement('@updatewordpress')
+         .setValue('@textboxwordpress', newwordpress)
+          .clickOnElement('@wordpresssave') 
+           .perform((done) => {
+              this.api.getAlertText((result) => {
+                  const alertText = result.value;
+                  if (alertText === 'Profile updated successfully!') {
+                      console.log('Profile updated successfully!');
+                  } else {
+                      console.log('Profile update failed. Alert text:', alertText);
+                  }
+                  done();
+              });
+        
+          })
+        .waitForPageLoad() 
+        
+        
+},
+verifyCompetencySelection() {
+    return this
+    .clickOnElement('@changecompetency')
+        .waitForElementVisible('@updatecompetency', 10000)
+        .clickOnElement('@updatecompetency')
+        .waitForElementVisible('@selectdropdown', 10000)
+        .clickOnElement('@selectdropdown')
+        .clickOnElement('@savedropdown')
+          .perform((done) => {
+              this.api.getAlertText((result) => {
+                  const alertText = result.value;
+                  if (alertText === 'Profile updated successfully!') {
+                      console.log('Profile updated successfully!');
+                  } else {
+                      console.log('Profile update failed. Alert text:', alertText);
+                  }
+                  done();
+              });      
+          })
+        .waitForPageLoad() 
+},
+verifyPracticeSelection() {
+    return this
+        .clickOnElement('@changepractice')
+         .clickOnElement('@updatetextpractice') 
+         .clickOnElement('@selectpractice')
+         .clickOnElement('@savebuttpractice')
+           .perform((done) => {
+               this.api.getAlertText((result) => {
+                   const alertText = result.value;
+                   if (alertText === 'Profile updated successfully!') {
+                       console.log('Profile updated successfully!');
+                   } else {
+                       console.log('Profile update failed. Alert text:', alertText);
+                   }
+                   done();
+               });      
+           })
+         .waitForPageLoad() 
+},
+verifyskill(skill) {
+    return this
+        .clickOnElement('@addskill')
+        .setValue('@addskill', skill) 
+         .setValue('@addskill', this.api.Keys.ENTER)
+            .perform((done) => {
+               this.api.getAlertText((result) => {
+                   const alertText = result.value;
+                   if (alertText === 'Successfully Updated') {
+                       console.log('Successfully Updated');
+                   } else {
+                       console.log('Profile update failed. Alert text:', alertText);
+                   }
+                   done();
+               });
+        
+           })
+         .waitForPageLoad() 
+        
+},
+verifySkillcopy(Skillcopy) {
+    return this
+         .clickOnElement('@addskill')
+         .setValue('@addskill', Skillcopy) 
+         .setValue('@addskill', this.api.Keys.ENTER)
+         .perform((done) => {
+            this.api.getAlertText((result) => {
+                const alertText = result.value;
+                if (alertText === 'Skill already added') {
+                    console.log('Skill already added');
+                } else {
+                    console.log('Profile update failed. Alert text:', alertText);
+                }
+                done();
+            });
+            
+        })
+        .waitForPageLoad() 
+      
+},
+verifyremoveskill() {
+    return this
+        .clickOnElement('@removeskill')
+            .perform((done) => {
+               this.api.getAlertText((result) => {
+                   const alertText = result.value;
+                   if (alertText === 'Successfully Updated') {
+                       console.log('Successfully Updated');
+                   } else {
+                       console.log('Profile update failed. Alert text:', alertText);
+                   }
+                   done();
+               });
+        
+           })
+         .waitForPageLoad() 
+        
+},
+verifyLocation() {
+    return this
+        .clickOnElement('@updatelocation')
+        .waitForPageLoad() 
+        .clickOnElement('@selectdroploc') 
+        .clickOnElement('@locup')
+        .clickOnElement('@saveloc')
+          .perform((done) => {
+              this.api.getAlertText((result) => {
+                  const alertText = result.value;
+                  if (alertText === 'Profile updated successfully!') {
+                      console.log('Profile updated successfully!');
+                  } else {
+                      console.log('Profile update failed. Alert text:', alertText);
+                  }
+                  done();
+              });      
+          })
+        .waitForPageLoad() 
+},
+verifylocinlist(){
+    return this
+    .clickOnElement('@updatelocation')
+        .setValue('@updatelocation', 'Bangalore')
+        .perform((done) => {
+            this.api.getAlertText((result) => {
+                const alertText = result.value;
+                if (alertText === 'Successfully Updated') {
+                    console.log('Successfully Updated');
+                } else {
+                    console.log('Profile update failed. Alert text:', alertText);
+                }
+                done();
+            });
+        })
+        .waitForPageLoad()
+        
+},
+verifylocblank(){
+    return this
+    .clickOnElement('@updatelocation')
+        .setValue('@updatelocation', '')
+        .perform((done) => {
+            this.api.getAlertText((result) => {
+                const alertText = result.value;
+                if (alertText === 'Successfully Updated') {
+                    console.log('Successfully Updated');
+                } else {
+                    console.log('Profile update failed . Alert text:', alertText);
+                }
+                done();
+            });
+        })
+        .waitForPageLoad()
+     
+
+},
+verifynamecantblank(){
+    return this
+        .clickOnElement('@editclickname')
+        .clearValue('@fullName') 
+        .setValue('@fullName', ' test ') 
+        .click('@savebutt')
+        
+        .waitForElementVisible('@errormsg', 5000)
+        .assert.containsText('@errormsg', 'Name can not contain whitespaces'.trim())  
+               
+        },   
+verifynamewithspecialchar(){
+            return this
+                .clickOnElement('@editclickname')
+                .clearValue('@fullName') 
+                .setValue('@fullName', ' te&%st ') 
+                .click('@savebutt')
+                .waitForElementVisible('@errorspe', 5000)
+                .assert.containsText('@errorspe', 'Name can only contain alphabets'.trim())  
+                       
+                },   
+verifynamewithblankvalue(){
+return this
+.clickOnElement('@editclickname')
+ .click('@savebutt')
+ .waitForElementVisible('@errormsg', 5000)
+ .assert.containsText('@errormsg', 'Name can not contain whitespaces'.trim())          
+                       },   
+verifywordpressclone(newwordpreess) {
+        return this
+             .clickOnElement('@updatewordpress')
+             .clickOnElement('@textboxwordpress')
+             .setValue('@textboxwordpress', newwordpreess)
+             .clickOnElement('@wordpresssave') 
+             .perform((done) => {
+                this.api.getAlertText((result) => {
+                    const alertText = result.value;
+                    if (alertText === 'Cannot update provided Wordpressid id , as it already belongs to other user') {
+                        console.log('Cannot update provided Wordpressid id  , as it already belongs to other user');
+                    } else {
+                        console.log('Profile update failed. Alert text:', alertText);
+                    }
+                    done();
+                });
+                
+            })
+            .waitForPageLoad()
+},                       
+}]
+ };
---
+++ go1percent/test/Go1PercentFEAutomation/setting/setting.js
@@ -0,0 +1,99 @@
+const globalsData = require('../../../globals');
+const settingPage = browser.page.settingPage.settingPage();
+describe("KNolx|Settingbutton", () => {
+    before(async function () {
+        browser
+            .maximizeWindow()
+            .page.login()
+            .navigate()
+            .enterCredentials(browser.globals.userName, browser.globals.password)
+            .signIn()
+    });
+it("should navigate to the dashboard", () => {
+        settingPage
+        .clickOn()
+});
+it("Verify user should be able to see all fields in Profile Details page", () => {
+     settingPage
+         .verifyAllFieldsVisible() 
+         
+});
+it("Verify user should be able to edit all the fields in Profile Details page", () => {
+    settingPage
+       .verifyeditname('Test emplyeeee')
+});
+it("Verify user should not be able to edit any field without click change or update button ", () => {
+    settingPage
+       .verifyChangesNot() 
+});
+it("Verify user should be able to provide only alphabets in the name field ", () => {
+    settingPage
+       .verifyAlphabetsNotall()
+});
+it("Verify user cannot not leave Github id and Wordpress id field blank ", () => {
+    settingPage
+    .verifyAllGitandWordpress()
+    
+});
+it("Verify user should provide valid Github id ", () => {
+    settingPage
+    .verifyGit('GitID')
+});
+it("Verify user should not be able to provide other users Github id  ", () => {
+    settingPage
+    .verifyGitclone('https://github.com/souravmishraa') 
+});
+it("Verify user should provide valid Wordpress id   ", () => {
+    settingPage
+    .verifywordpress('wordpressnew')
+});
+it("Verify user should be able to select any Competency   ", () => {
+    settingPage 
+    .verifyCompetencySelection()
+});
+it("Verify user should be able to select any component from Practice   ", () => {
+    settingPage
+      .verifyPracticeSelection()
+  
+});
+it("Verify user should be able to update skills    ", () => {
+    settingPage
+      .verifyskill('Selenium')
+});
+it("Verify user should not be able to enter same skill more than one time    ", () => {
+    settingPage
+     .verifySkillcopy('Selenium')
+});
+it("Verify user should be able to remove skills     ", () => {
+    settingPage
+     .verifyremoveskill()
+});
+it("Verify that user should be able to update location     ", () => {
+    settingPage
+    .verifyLocation()
+});
+it("verify that user should be able to select any component from location     ", () => {
+    settingPage
+    .verifylocinlist()
+});
+it("verify that user cannot leave location field empty     ", () => {
+    settingPage
+    .verifylocblank()
+});
+it("verify that user should not be able to add white space before or after the name in the name filed      ", () => {
+    settingPage
+    .verifynamecantblank()
+});
+it("verify that user should not be able to add special characters in the name in the name filed      ", () => {
+    settingPage
+    .verifynamewithspecialchar()
+});
+it("verify that user cannot leave Full name field empty      ", () => {
+    settingPage
+    .verifynamewithblankvalue()
+});
+it("verify that user cannot leave Full name field empty      ", () => {
+    settingPage
+    .verifywordpressclone('dummy8e296154-7caf-4ca5-a1d5-3f4f19a2cdca') 
+});
+});
